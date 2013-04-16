0f6fcc8ee160015cb96e089f21846d9ce5ae2c53
Erik van Ingen <erikvaningen@gmail.com>
2013-04-16 16:31:18 +0200

    tell

--- vme-service/src/main/java/org/fao/fi/vme/dao/VmeDao.java
+++ vme-service/src/main/java/org/fao/fi/vme/dao/VmeDao.java
@@ -3,6 +3,7 @@ package org.fao.fi.vme.dao;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 import org.fao.fi.vme.domain.Vme;
 
@@ -13,6 +14,10 @@ public class VmeDao {
 	private EntityManager em;
 
 	public void persistVme(Vme vme) {
+		EntityTransaction t = em.getTransaction();
+		t.begin();
 		em.persist(vme);
+		t.commit();
+		em.close();
 	}
 }
