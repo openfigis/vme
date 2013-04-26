be05375d22de1fa820e6166950dc9fcaf5c37325
Erik van Ingen <erikvaningen@gmail.com>
2013-04-26 12:43:53 +0200

    linking meetings with rfmo's

--- vme-service/src/main/java/org/fao/fi/vme/dao/msaccess/Linker.java
+++ vme-service/src/main/java/org/fao/fi/vme/dao/msaccess/Linker.java
@@ -3,6 +3,10 @@ package org.fao.fi.vme.dao.msaccess;
 import java.util.List;
 import java.util.Map;
 
+import org.fao.fi.vme.dao.msaccess.tables.Meetings;
+import org.fao.fi.vme.dao.msaccess.tables.VME;
+import org.fao.fi.vme.domain.Meeting;
+import org.fao.fi.vme.domain.Rfmo;
 import org.fao.fi.vme.domain.Vme;
 
 public class Linker {
@@ -19,23 +23,24 @@ public class Linker {
 			Map<Object, Object> domainTableMap = o.getDomainTableMap();
 			List<Object> objectList = o.getObjectList();
 			for (Object object : objectList) {
-				linkObject(object, objectCollectionList, tables);
+				linkObject(object, domainTableMap, objectCollectionList, tables);
 			}
 
 		}
 
 	}
 
-	private void linkObject(Object domainObject, List<ObjectCollection> objectCollectionList, List<Table> tables) {
+	private void linkObject(Object domainObject, Map<Object, Object> domainTableMap,
+			List<ObjectCollection> objectCollectionList, List<Table> tables) {
 		if (domainObject instanceof Vme) {
-			linkVmeObject(domainObject, objectCollectionList, tables);
+			linkVmeObject(domainObject, domainTableMap, objectCollectionList);
 		}
 		// if (domainObject instanceof Rfmo) {
 		// linkRfmoObject(domainObject, objectCollectionList, tables);
 		// }
-		// if (domainObject instanceof Meeting) {
-		// linkVmeObject(domainObject, objectCollectionList, tables);
-		// }
+		if (domainObject instanceof Meeting) {
+			linkMeetingObject(domainObject, domainTableMap, objectCollectionList);
+		}
 		// if (domainObject instanceof SpecificMeasures) {
 		// linkSpecificMeasuresObject(domainObject, objectCollectionList, tables);
 		// }
@@ -45,13 +50,59 @@ public class Linker {
 
 	}
 
-	private void linkVmeObject(Object vmeObject, List<ObjectCollection> objectCollectionList, List<Table> tables) {
-		Vme vme = (Vme) vmeObject;
-
-		// linking the RFMO
-		for (Table table : tables) {
-
+	/**
+	 * filling up rfmo's with meeting
+	 * 
+	 * 
+	 * @param meetingDomainObject
+	 * @param domainTableMap
+	 * @param objectCollectionList
+	 */
+	private void linkMeetingObject(Object meetingDomainObject, Map<Object, Object> domainTableMap,
+			List<ObjectCollection> objectCollectionList) {
+		Meeting meeting = (Meeting) meetingDomainObject;
+		Meetings meetingsRecord = (Meetings) domainTableMap.get(meeting);
+		for (ObjectCollection oc : objectCollectionList) {
+			if (oc.getClazz() == Rfmo.class) {
+				List<Object> objectList = oc.getObjectList();
+				for (Object object : objectList) {
+					Rfmo o = (Rfmo) object;
+					int rfbId = (new Integer(meetingsRecord.getRFB_ID())).intValue();
+					if (o.getId() == rfbId) {
+						o.getMeetingList().add(meeting);
+					}
+				}
+			}
 		}
 
+	}
+
+	/**
+	 * fills up Vme with a Rfmo and the other way around.
+	 * 
+	 * 
+	 * @param vmeObject
+	 * @param domainTableMap
+	 * @param objectCollectionList
+	 */
+	private void linkVmeObject(Object vmeObject, Map<Object, Object> domainTableMap,
+			List<ObjectCollection> objectCollectionList) {
+		Vme vme = (Vme) vmeObject;
+		VME vmeRecord = (VME) domainTableMap.get(vme);
+		for (ObjectCollection oc : objectCollectionList) {
+			if (oc.getClazz() == Rfmo.class) {
+				List<Object> objectList = oc.getObjectList();
+				for (Object object : objectList) {
+					Rfmo o = (Rfmo) object;
+					int rfbId = (new Integer(vmeRecord.getRFB_ID())).intValue();
+					if (o.getId() == rfbId) {
+						vme.setRfmo(o);
+						if (!o.getManagedVmeList().contains(o)) {
+							o.getManagedVmeList().add(vme);
+						}
+					}
+				}
+			}
+		}
 	}
 }
