d141a031597c3d0183272074a1ca48cbcc909c70
Erik van Ingen <erikvaningen@gmail.com>
2013-04-26 10:35:55 +0200

    preparing the linking of objects

---
+++ vme-service/src/test/java/org/fao/fi/vme/dao/msaccess/LinkerTest.java
@@ -0,0 +1,100 @@
+package org.fao.fi.vme.dao.msaccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.fao.fi.vme.dao.config.EntityManagerFactoryProducer;
+import org.fao.fi.vme.dao.config.EntityManagerProducer;
+import org.fao.fi.vme.domain.GeneralMeasures;
+import org.fao.fi.vme.domain.Meeting;
+import org.fao.fi.vme.domain.Rfmo;
+import org.fao.fi.vme.domain.SpecificMeasures;
+import org.fao.fi.vme.domain.Vme;
+import org.jglue.cdiunit.ActivatedAlternatives;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(CdiRunner.class)
+@ActivatedAlternatives({ EntityManagerFactoryProducer.class, EntityManagerProducer.class })
+public class LinkerTest {
+
+	@Inject
+	private VmeWriter writer;
+
+	private final MsAcces2DomainMapper m = new MsAcces2DomainMapper();
+	private final Linker linker = new Linker();
+
+	private final VmeReader reader = new VmeReader();
+
+	@Test
+	public void testLink() {
+		// read from MSAccess
+		List<Table> tables = reader.readObjects();
+
+		// convert the table objects to domain objects
+		List<ObjectCollection> objectCollectionList = new ArrayList<ObjectCollection>();
+		for (Table table : tables) {
+			objectCollectionList.add(m.map(table));
+		}
+
+		// connect/link the domain objects, also using the original information from the table objects
+		linker.link(objectCollectionList, tables);
+
+		for (ObjectCollection objectCollection : objectCollectionList) {
+			List<Object> objects = objectCollection.getObjectList();
+			for (Object object : objects) {
+				if (object instanceof Vme) {
+					validateVmeObject(object);
+				}
+				if (object instanceof Rfmo) {
+					validateRfmoObject(object);
+				}
+				if (object instanceof Meeting) {
+					validateVmeObject(object);
+				}
+				if (object instanceof SpecificMeasures) {
+					validateSpecificMeasuresObject(object);
+				}
+				if (object instanceof GeneralMeasures) {
+					validateGeneralMeasuresObject(object);
+				}
+
+			}
+
+		}
+
+	}
+
+	private void validateVmeObject(Object object) {
+		Vme o = (Vme) object;
+		assertNotNull(o.getRfmo().getId());
+
+	}
+
+	private void validateRfmoObject(Object object) {
+		Rfmo o = (Rfmo) object;
+		assertTrue(o.getFishingActivityList().size() > 0);
+		assertTrue(o.getGeneralMeasuresList().size() > 0);
+		assertTrue(o.getManagedVmeList().size() > 0);
+		assertTrue(o.getMeetingList().size() > 0);
+	}
+
+	private void validateGeneralMeasuresObject(Object object) {
+		GeneralMeasures o = (GeneralMeasures) object;
+		assertNotNull(o.getPrimairlyConcernedVme());
+		assertNotNull(o.getRfmo());
+
+	}
+
+	private void validateSpecificMeasuresObject(Object object) {
+		SpecificMeasures o = (SpecificMeasures) object;
+		assertNotNull(o.getVme());
+	}
+
+}
