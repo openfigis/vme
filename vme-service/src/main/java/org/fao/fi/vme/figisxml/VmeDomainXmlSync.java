f493318306b350e9273cedd53472e982fde0ba25
Erik van Ingen <erikvaningen@gmail.com>
2013-05-30 09:45:14 +0200

    new project to start modeling figis as a domain, using hibernate

---
+++ vme-service/src/main/java/org/fao/fi/vme/figisxml/VmeDomainXmlSync.java
@@ -0,0 +1,14 @@
+package org.fao.fi.vme.figisxml;
+
+/**
+ * Syncs the FIGIS Factsheet XML with the information from the VME domain model.
+ * 
+ * @author Erik van Ingen
+ * 
+ */
+public class VmeDomainXmlSync {
+
+	public void updateFigisVmeXml() {
+	}
+
+}
