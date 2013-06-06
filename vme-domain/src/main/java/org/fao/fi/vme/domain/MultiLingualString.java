a65291205609f60cef9d89d8509b9f704db22dcd
Erik van Ingen <erikvaningen@gmail.com>
2013-06-06 17:40:55 +0200

    adding comments for Fabrizio

--- vme-domain/src/main/java/org/fao/fi/vme/domain/MultiLingualString.java
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/MultiLingualString.java
@@ -2,15 +2,48 @@ package org.fao.fi.vme.domain;
 
 import java.util.Map;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Not applied yet.
  * 
+ * See also http://stackoverflow.com/questions/5964027/localizable-strings-in-jpa
+ * 
+ * 
+ * http://hwellmann.blogspot.it/2010/07/jpa-20-querying-map.html
+ * 
+ * http://hwellmann.blogspot.it/2010/07/jpa-20-mapping-map.html
  * 
  * @author Erik van Ingen
  * 
  */
+@Entity
 public class MultiLingualString {
 
+	@Id
+	private long id;
+
+	/**
+	 * map of descriptions per language
+	 * 
+	 */
 	private Map<Integer, String> stringMap;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Map<Integer, String> getStringMap() {
+		return stringMap;
+	}
+
+	public void setStringMap(Map<Integer, String> stringMap) {
+		this.stringMap = stringMap;
+	}
 
 }
