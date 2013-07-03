0af5f4b26d036a47a6cecdd26af78f2ca3f1713a
Erik van Ingen <erikvaningen@gmail.com>
2013-07-03 13:52:34 +0200

    creating drop table script for VME

---
+++ vme-sql/dropVmeTables.sql
@@ -0,0 +1,26 @@
+drop table information_source_rfmo CASCADE CONSTRAINTS;
+drop table rfmo_information_source CASCADE CONSTRAINTS;
+drop table rfmo_history CASCADE CONSTRAINTS;
+drop table vme_geo_ref CASCADE CONSTRAINTS;
+drop table geo_ref CASCADE CONSTRAINTS;
+drop table vme_history CASCADE CONSTRAINTS;
+drop table vme_specific_measures CASCADE CONSTRAINTS;
+drop table history CASCADE CONSTRAINTS;
+drop table specific_measures CASCADE CONSTRAINTS;
+drop table general_measures CASCADE CONSTRAINTS;
+drop table vme_profile CASCADE CONSTRAINTS;
+drop table vme;
+drop table profile;
+drop table rfmo;
+
+
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'GENERAL_MEASURES';
+
+delete  USER_CONSTRAINTS
+where r_owner = 'VME_ADMIN'
+
+
+SELECT table_name FROM USER_CONSTRAINTS
+where r_owner = 'VME_ADMIN'
+
+
