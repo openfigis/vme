88770e611a3862bea26064a3779b1857852f0048
Erik van Ingen <erikvaningen@gmail.com>
2013-07-09 17:03:41 +0200

    Adding TemporaryBatch

---
+++ vme-service-integrationtest/src/test/java/org/fao/fi/vme/sync0/TemporaryBatchTest.java
@@ -0,0 +1,23 @@
+package org.fao.fi.vme.sync0;
+
+import javax.inject.Inject;
+
+import org.fao.fi.vme.dao.config.FigisDataBaseProducer;
+import org.fao.fi.vme.dao.config.VmeDataBaseProducer;
+import org.jglue.cdiunit.ActivatedAlternatives;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+@ActivatedAlternatives({ VmeDataBaseProducer.class, FigisDataBaseProducer.class })
+public class TemporaryBatchTest {
+
+	@Inject
+	TemporaryBatch temporaryBatch;
+
+	@Test
+	public void testRun() {
+		temporaryBatch.run();
+	}
+}
