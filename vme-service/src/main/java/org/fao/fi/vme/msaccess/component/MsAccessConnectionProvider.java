a3fb183a42bb78ee60edb17a1c7e9dbabcdbfc0d
Sibeni <fabrizio@ilfiammifero.it>
2013-07-11 11:41:01 +0200

    Integration with UcanAccess in MsAccess Connection provider

--- vme-service/src/main/java/org/fao/fi/vme/msaccess/component/MsAccessConnectionProvider.java
+++ vme-service/src/main/java/org/fao/fi/vme/msaccess/component/MsAccessConnectionProvider.java
@@ -8,15 +8,23 @@ import org.fao.fi.vme.VmeException;
 
 public class MsAccessConnectionProvider {
 
+	public static final String JDBC_ODBC_DRIVER = "sun.jdbc.odbc.JdbcOdbcDriver";
+	public static final String UCANACCESS_DRIVER = "net.ucanaccess.jdbc.UcanaccessDriver";
+	
+	public static final String JDBC_ODBC_URL = "jdbc:odbc:Driver={Microsoft Access Driver " + "(*.mdb, *.accdb)};DBQ=";
+	public static final String UCANACCESS_URL = "jdbc:ucanaccess://";
+	
+	
+	
 	Connection connecton;
 
 	String dbLocation = "../vme-service-integrationtest/src/test/resources/VME_DBTest-FC.accdb";
 
 	public MsAccessConnectionProvider() {
 		try {
-			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-			String url = "jdbc:odbc:Driver={Microsoft Access Driver " + "(*.mdb, *.accdb)};DBQ=" + dbLocation;
-			System.out.println("Connected!");
+			Class.forName(UCANACCESS_DRIVER);
+			String url = UCANACCESS_URL + dbLocation;
+			System.out.println("Microsoft Access file : " +  dbLocation + "successfully connected!");
 			this.connecton = DriverManager.getConnection(url);
 		} catch (SQLException e) {
 			throw new VmeException(e);
