cc678de478b1f0f844d1634e41ea8403d7eb2e3c
Erik van Ingen <erikvaningen@gmail.com>
2013-07-12 09:21:03 +0200

    simplifying test

--- vme-fimes-jaxb/src/test/java/org/fao/fi/figis/devcon/FIGISDocTest.java
+++ vme-fimes-jaxb/src/test/java/org/fao/fi/figis/devcon/FIGISDocTest.java
@@ -29,13 +29,84 @@ public class FIGISDocTest extends XMLTestCase {
 	File file = new File("src/test/resources/VMEFactSheet_XML_prototype.xml");
 	File fileShort = new File("src/test/resources/VmeShort.xml");
 
-	FIGISDoc expectedResult = null;
+	// FIGISDoc expectedResult = null;
 
 	java.lang.Class<FIGISDoc> clazz = FIGISDoc.class;
 
-	public FIGISDocTest() {
-		expectedResult = new FIGISDoc();
+	/**
+	 * Marshall is from java to XML, which we need for the project.
+	 * 
+	 * 
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws JAXBException
+	 */
+	@Test
+	public void testMarshall() throws SAXException, IOException, JAXBException {
+		FIGISDoc expectedResult = prepareExpectedResult();
 
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Writer out = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+		// set up JAXB marshalling context
+		JAXBContext context = JAXBContext.newInstance(clazz);
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		// this property works fine with jdk1.6.0_16
+		// marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new FimesNamespacePrefixMapper());
+		marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new FimesNamespacePrefixMapper());
+
+		marshaller.marshal(expectedResult, out);
+
+		// marshal to console - nice if you're looking
+		marshaller.marshal(expectedResult, new BufferedWriter(new OutputStreamWriter(System.out)));
+
+		// convert to InputSource for the comparison test
+		InputSource marshallingResult = new InputSource(new ByteArrayInputStream(outputStream.toByteArray()));
+
+		// get expected result from filesystem
+		InputSource expectedResultFile = new InputSource(new FileInputStream(fileShort));
+
+		// comparison test
+		Diff diff = new Diff(expectedResultFile, marshallingResult);
+		diff.overrideDifferenceListener(new IgnoreTextAndAttributeValuesDifferenceListener());
+		assertTrue("Marshalled Object matches expected XML " + diff, diff.similar());
+	}
+
+	/**
+	 * Unmarshall is from xml to java. Does not work yet, there is a problem with illegal annotations. For the project
+	 * we only need from java to xml.
+	 * 
+	 * @throws JAXBException
+	 */
+	public void _testUnmarshall() throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(clazz);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		JaxbValidationEventHandler handler = new JaxbValidationEventHandler();
+		unmarshaller.setEventHandler(handler);
+
+		Object object = unmarshaller.unmarshal(file);
+		if (!handler.getMessages().isEmpty()) {
+			String errorMessage = "XML parse errors:";
+			for (Object message : handler.getMessages()) {
+				errorMessage += "\n" + message;
+			}
+			throw new JAXBException(errorMessage);
+		}
+
+		// test equality
+		// Equality cannot be tested like this because the equals of the jaxb
+		// objects cannot be changed. Therefore it
+		// will be tested on not null
+		// assertEquals("Unmarshalling object", expectedResult, object);
+		assertNotNull(object);
+	}
+
+	/**
+	 * preparing the expected result in the
+	 */
+	public FIGISDoc prepareExpectedResult() {
 		VMEIdent vmeIdent = new VMEIdent();
 
 		FigisID figisID = new FigisID();
@@ -69,78 +140,8 @@ public class FIGISDocTest extends XMLTestCase {
 		FIGISDoc figisDoc = new FIGISDoc();
 		figisDoc.setVME(vme);
 
-		this.expectedResult = figisDoc;
+		return figisDoc;
 
-	}
-
-	/**
-	 * Marshall is from java to XML, which we need for the project.
-	 * 
-	 * 
-	 * @throws SAXException
-	 * @throws IOException
-	 * @throws JAXBException
-	 */
-	@Test
-	public void testMarshall() throws SAXException, IOException, JAXBException {
-		assertNotNull(expectedResult);
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		Writer out = new BufferedWriter(new OutputStreamWriter(outputStream));
-
-		// set up JAXB marshalling context
-		JAXBContext context = JAXBContext.newInstance(clazz);
-		Marshaller marshaller = context.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		// this property works fine with jdk1.6.0_16
-		// marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new FimesNamespacePrefixMapper());
-		marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new FimesNamespacePrefixMapper());
-
-		marshaller.marshal(expectedResult, out);
-
-		// marshal to console - nice if you're looking
-		marshaller.marshal(expectedResult, new BufferedWriter(new OutputStreamWriter(System.out)));
-
-		// convert to InputSource for the comparison test
-		InputSource marshallingResult = new InputSource(new ByteArrayInputStream(outputStream.toByteArray()));
-
-		// get expected result from filesystem
-		InputSource expectedResult = new InputSource(new FileInputStream(fileShort));
-
-		// comparison test
-		Diff diff = new Diff(expectedResult, marshallingResult);
-		diff.overrideDifferenceListener(new IgnoreTextAndAttributeValuesDifferenceListener());
-		assertTrue("Marshalled Object matches expected XML " + diff, diff.similar());
-	}
-
-	/**
-	 * Unmarshall is from xml to java. Does not work yet, there is a problem with illegal annotations. For the project
-	 * we only need from java to xml.
-	 * 
-	 * @throws JAXBException
-	 */
-	public void _testUnmarshall() throws JAXBException {
-		JAXBContext context = JAXBContext.newInstance(clazz);
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-
-		JaxbValidationEventHandler handler = new JaxbValidationEventHandler();
-		unmarshaller.setEventHandler(handler);
-
-		Object object = unmarshaller.unmarshal(file);
-		if (!handler.getMessages().isEmpty()) {
-			String errorMessage = "XML parse errors:";
-			for (Object message : handler.getMessages()) {
-				errorMessage += "\n" + message;
-			}
-			throw new JAXBException(errorMessage);
-		}
-
-		// test equality
-		// Equality cannot be tested like this because the equals of the jaxb
-		// objects cannot be changed. Therefore it
-		// will be tested on not null
-		// assertEquals("Unmarshalling object", expectedResult, object);
-		assertNotNull(object);
 	}
 
 }
