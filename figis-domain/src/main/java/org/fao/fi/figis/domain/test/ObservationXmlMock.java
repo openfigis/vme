f12c889d9528ba7d48749b97a3f4c5ddcfdd6264
Erik van Ingen <erikvaningen@gmail.com>
2013-07-13 14:32:53 +0200

    adding tests for real xml in the observationXml

--- figis-domain/src/main/java/org/fao/fi/figis/domain/test/ObservationXmlMock.java
+++ figis-domain/src/main/java/org/fao/fi/figis/domain/test/ObservationXmlMock.java
@@ -2,8 +2,10 @@ package org.fao.fi.figis.domain.test;
 
 import java.sql.Date;
 
+import org.fao.fi.figis.devcon.FIGISDoc;
 import org.fao.fi.figis.domain.ObservationXml;
 import org.fao.fi.figis.domain.rule.Figis;
+import org.vme.fimes.jaxb.JaxbMarshall;
 
 public class ObservationXmlMock {
 	public static ObservationXml create() {
@@ -14,7 +16,9 @@ public class ObservationXmlMock {
 		Date d = new Date(System.currentTimeMillis());
 		xml.setLastEditDate(d);
 		xml.setCreationDate(d);
-
+		FIGISDoc figisDoc = new FIGISDoc();
+		JaxbMarshall m = new JaxbMarshall();
+		xml.setXml(m.marshalToString(figisDoc));
 		return xml;
 	}
 }
