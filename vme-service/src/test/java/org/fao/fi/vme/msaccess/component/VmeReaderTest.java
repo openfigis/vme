15a0cbcfdbd78e7434b06aed14f9b81b027619f4
Erik van Ingen <erikvaningen@gmail.com>
2013-07-16 11:13:33 +0200

    counting the number of  records in the vme and rfmo table.

--- vme-service/src/test/java/org/fao/fi/vme/msaccess/component/VmeReaderTest.java
+++ vme-service/src/test/java/org/fao/fi/vme/msaccess/component/VmeReaderTest.java
@@ -1,20 +1,31 @@
 package org.fao.fi.vme.msaccess.component;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.fao.fi.vme.msaccess.model.Table;
+import org.fao.fi.vme.msaccess.tables.RFB_MetaData;
+import org.fao.fi.vme.msaccess.tables.VME;
 import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class VmeReaderTest {
 
 	VmeReader reader = new VmeReader();
+
+	public static Map<Class<?>, Integer> numberOfRecords = new HashMap<Class<?>, Integer>();
+	static {
+		numberOfRecords.put(VME.class, 264);
+		numberOfRecords.put(RFB_MetaData.class, 5);
+	}
 
 	@Test
 	public void testReadObjects() throws SecurityException, NoSuchMethodException, IllegalArgumentException,
@@ -24,6 +35,11 @@ public class VmeReaderTest {
 			// System.out.println(table.getClazz().getSimpleName());
 			assertTrue(table.getObjectList().size() > 2);
 			Set<String> ids = new HashSet<String>();
+
+			Class<?> tableClazz = table.getClazz();
+			if (numberOfRecords.containsKey(table.getClazz())) {
+				assertEquals(numberOfRecords.get(tableClazz).intValue(), table.getObjectList().size());
+			}
 
 			// checking unicity
 			for (Object object : table.getObjectList()) {
