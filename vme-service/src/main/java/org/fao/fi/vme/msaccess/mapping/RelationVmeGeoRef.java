5287ab27e17faccaf49e3e7089e9c6bcef3dfca9
Erik van Ingen <erikvaningen@gmail.com>
2013-07-16 16:02:47 +0200

    Removing doubles from the georefs

--- vme-service/src/main/java/org/fao/fi/vme/msaccess/mapping/RelationVmeGeoRef.java
+++ vme-service/src/main/java/org/fao/fi/vme/msaccess/mapping/RelationVmeGeoRef.java
@@ -9,6 +9,12 @@ import org.fao.fi.vme.domain.GeoRef;
 import org.fao.fi.vme.domain.Vme;
 import org.fao.fi.vme.msaccess.model.ObjectCollection;
 
+/**
+ * This relation is in the vme table itself, therefore it can not be established in the table itself.
+ * 
+ * @author vaningen
+ * 
+ */
 public class RelationVmeGeoRef {
 
 	public void correct(List<ObjectCollection> objectCollectionList) {
@@ -32,7 +38,6 @@ public class RelationVmeGeoRef {
 		Map<String, Vme> vmeMap = new HashMap<String, Vme>();
 		for (Object object : vmeList) {
 			Vme vme = (Vme) object;
-
 			String key = vme.getInventoryIdentifier();
 			if (vmeMap.containsKey(key)) {
 				Vme vmeTarget = vmeMap.get(key);
@@ -40,32 +45,28 @@ public class RelationVmeGeoRef {
 				doubles.add(vme);
 			} else {
 				vmeMap.put(key, vme);
-				workToDoGeoRef(vme);
 			}
 		}
-		// remove the doubles
+		// remove the doubles vme's
 		for (Object object : doubles) {
 			objectCollection.getObjectList().remove(object);
 		}
 
-	}
-
-	private void workToDoGeoRef(Vme vme) {
-		Map<String, GeoRef> geoRefMap = new HashMap<String, GeoRef>();
-		List<GeoRef> doubles = new ArrayList<GeoRef>();
-		List<GeoRef> geoRefList = vme.getGeoRefList();
-		for (GeoRef geoRef : geoRefList) {
-			String key = geoRef.getGeographicFeatureID();
-			if (geoRefMap.containsKey(key)) {
-				GeoRef geoRefTarget = geoRefMap.get(key);
-				doubles.add(geoRefTarget);
-			} else {
-				geoRefMap.put(key, geoRef);
+		// remove the double georefs.
+		Map<String, GeoRef> geoRefmap = new HashMap<String, GeoRef>();
+		for (Object object : vmeList) {
+			List<GeoRef> doubleGeoRefs = new ArrayList<GeoRef>();
+			List<GeoRef> list = ((Vme) object).getGeoRefList();
+			for (GeoRef geoRef : list) {
+				if (geoRefmap.containsKey(geoRef.getGeographicFeatureID())) {
+					doubleGeoRefs.add(geoRef);
+				} else {
+					geoRefmap.put(geoRef.getGeographicFeatureID(), geoRef);
+				}
 			}
-		}
-		// remove the doubles
-		for (GeoRef object : doubles) {
-			vme.getGeoRefList().remove(object);
+			for (GeoRef geoRef : doubleGeoRefs) {
+				((Vme) object).getGeoRefList().remove(geoRef);
+			}
 		}
 
 	}
--- vme-service/src/test/java/org/fao/fi/vme/msaccess/mapping/RelationVmeGeoRefTest.java
+++ vme-service/src/test/java/org/fao/fi/vme/msaccess/mapping/RelationVmeGeoRefTest.java
@@ -18,16 +18,22 @@ public class RelationVmeGeoRefTest {
 	public void testCorrectVme() {
 
 		String inventoryIdentifier = "567893423q";
+		String geographicFeatureID = "hfsdkfhsefh";
 
 		Vme v1 = new Vme();
 		v1.setInventoryIdentifier(inventoryIdentifier);
+
 		Vme v2 = new Vme();
 		v2.setInventoryIdentifier(inventoryIdentifier);
 		GeoRef g1 = new GeoRef();
+		g1.setGeographicFeatureID(geographicFeatureID);
 		GeoRef g2 = new GeoRef();
+		g2.setGeographicFeatureID(geographicFeatureID);
+
 		List<GeoRef> list1 = new ArrayList<GeoRef>();
 		list1.add(g1);
 		v1.setGeoRefList(list1);
+
 		List<GeoRef> list2 = new ArrayList<GeoRef>();
 		list2.add(g2);
 		v2.setGeoRefList(list2);
@@ -46,39 +52,7 @@ public class RelationVmeGeoRefTest {
 		assertEquals(2, objectList.size());
 		correction.correct(objectCollectionList);
 		assertEquals(1, objectList.size());
+		Vme vme = (Vme) objectList.get(0);
+		assertEquals(1, vme.getGeoRefList().size());
 	}
-
-	@Test
-	public void testCorrectGeoRef() {
-
-		String inventoryIdentifier = "567893423q";
-
-		Vme v1 = new Vme();
-		v1.setInventoryIdentifier(inventoryIdentifier);
-		GeoRef g1 = new GeoRef();
-		g1.setGeographicFeatureID(inventoryIdentifier);
-		GeoRef g2 = new GeoRef();
-		g2.setGeographicFeatureID(inventoryIdentifier);
-		List<GeoRef> list1 = new ArrayList<GeoRef>();
-		list1.add(g1);
-		list1.add(g2);
-		v1.setGeoRefList(list1);
-
-		List<Object> objectList = new ArrayList<Object>();
-		objectList.add(v1);
-
-		ObjectCollection oc = new ObjectCollection();
-		oc.setClazz(Vme.class);
-		oc.setObjectList(objectList);
-
-		List<ObjectCollection> objectCollectionList = new ArrayList<ObjectCollection>();
-		objectCollectionList.add(oc);
-
-		assertEquals(1, objectList.size());
-		assertEquals(2, v1.getGeoRefList().size());
-		correction.correct(objectCollectionList);
-		assertEquals(1, objectList.size());
-		assertEquals(1, v1.getGeoRefList().size());
-	}
-
 }
