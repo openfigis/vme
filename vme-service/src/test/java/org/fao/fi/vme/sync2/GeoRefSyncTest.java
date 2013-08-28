7037d6272bd969805bce3ece436328166db76fc2
Erik van Ingen <erikvaningen@gmail.com>
2013-08-28 09:01:56 +0200

    testing the behavior of the sunced georefs with water area refs

---
+++ vme-service/src/test/java/org/fao/fi/vme/sync2/GeoRefSyncTest.java
@@ -0,0 +1,43 @@
+package org.fao.fi.vme.sync2;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.fao.fi.figis.dao.FigisDao;
+import org.fao.fi.figis.domain.RefWaterArea;
+import org.fao.fi.vme.dao.VmeDao;
+import org.fao.fi.vme.dao.config.FigisDataBaseProducer;
+import org.fao.fi.vme.dao.config.VmeDataBaseProducer;
+import org.fao.fi.vme.domain.GeoRef;
+import org.jglue.cdiunit.ActivatedAlternatives;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+@ActivatedAlternatives({ VmeDataBaseProducer.class, FigisDataBaseProducer.class })
+public class GeoRefSyncTest {
+
+	@Inject
+	FigisDao figisDao;
+
+	@Inject
+	VmeDao vmeDao;
+
+	@Inject
+	GeoRefSync geoRefSync;
+
+	@Test
+	public void testSync() {
+		String geographicFeatureID = "VME_X";
+		GeoRef geoRef = new GeoRef();
+		geoRef.setGeographicFeatureID(geographicFeatureID);
+		vmeDao.persist(geoRef);
+		int x = figisDao.loadObjects(RefWaterArea.class).size();
+		geoRefSync.sync();
+		assertEquals(++x, figisDao.loadObjects(RefWaterArea.class).size());
+		geoRefSync.sync();
+		assertEquals(x, figisDao.loadObjects(RefWaterArea.class).size());
+	}
+}
