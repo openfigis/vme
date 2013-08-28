4c08f86b1d135b250a3d44669285e313efe3dd3d
Erik van Ingen <erikvaningen@gmail.com>
2013-08-28 10:38:30 +0200

    Testing the formats

---
+++ vme-service/src/test/java/org/fao/fi/vme/sync2/mapping/xml/DateFormatterTest.java
@@ -0,0 +1,19 @@
+package org.fao.fi.vme.sync2.mapping.xml;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+public class DateFormatterTest {
+
+	DateFormatter df = new DateFormatter();
+
+	@Test
+	public void testFormatDateDate() {
+		System.out.println(df.format(Calendar.getInstance().getTime(), Calendar.getInstance().getTime()));
+		assertEquals(25, df.format(Calendar.getInstance().getTime(), Calendar.getInstance().getTime()).length());
+	}
+
+}
