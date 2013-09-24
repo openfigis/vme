a64a7a4f9d4a16c2c9906bdfd929590586f9a519
Erik van Ingen <erikvaningen@gmail.com>
2013-09-24 13:46:16 +0200

    Implementing/correcting History of Fishery Areas and VME history

---
+++ vme-service/src/main/java/org/fao/fi/vme/msaccess/tableextension/HistoryHolder.java
@@ -0,0 +1,34 @@
+package org.fao.fi.vme.msaccess.tableextension;
+
+import org.fao.fi.vme.domain.History;
+
+/**
+ * The pattern is that every table in MS-Acces can be mapped to one domain object. This case is an exception where the
+ * Access table RFB_VME_Fishing_History is mapped to 2 tables.
+ * 
+ * 
+ * @author Erik van Ingen
+ * 
+ */
+public class HistoryHolder {
+
+	private History fisheryAreasHistory;
+	private History vmesHistory;
+
+	public History getFisheryAreasHistory() {
+		return fisheryAreasHistory;
+	}
+
+	public void setFisheryAreasHistory(History fisheryAreasHistory) {
+		this.fisheryAreasHistory = fisheryAreasHistory;
+	}
+
+	public History getVmesHistory() {
+		return vmesHistory;
+	}
+
+	public void setVmesHistory(History vmesHistory) {
+		this.vmesHistory = vmesHistory;
+	}
+
+}
