2ac9986d92cad7c2642e5fc9473c24182613ee25
Fabio Fiorellato <fabio.fiorellato@fao.org>
2013-11-25 02:23:23 +0100

    ProfileMocker added.

---
+++ vme-reports-store-gateway-test/src/main/java/org/vme/test/mock/ProfileMocker.java
@@ -0,0 +1,42 @@
+/**
+ * (c) 2013 FAO / UN (project: vme-reports-store-gateway-test)
+ */
+package org.vme.test.mock;
+
+import org.fao.fi.vme.domain.model.Profile;
+
+/**
+ * Place your class / interface description here.
+ *
+ * History:
+ *
+ * ------------- --------------- -----------------------
+ * Date			 Author			 Comment
+ * ------------- --------------- -----------------------
+ * 25/nov/2013   Fabio     Creation.
+ *
+ * @version 1.0
+ * @since 25/nov/2013
+ */
+public class ProfileMocker extends AbstractMocker {
+	static public Profile getMock1() {
+		Profile profile = new Profile();
+		profile.setId(1L);
+		
+		profile.setDescriptionImpact(MLSu.english("Carpe diem"));
+		profile.setDescriptionPhisical(MLSu.english("Retro se recepit"));
+		
+		return profile;
+	}
+	
+	static public Profile getMock2() {
+		Profile profile = new Profile();
+		profile.setId(2L);
+		
+		profile.setDescriptionImpact(MLSu.english("Da mi basia mille deinde centum"));
+		profile.setDescriptionPhisical(MLSu.english("Tytire tu patulae, recubans sub tegmine fagi"));
+		profile.setDescriptionBiological(MLSu.english("Dulce et decorum est pro Claudia Mori"));
+
+		return profile;
+	}
+}
