b8db0606dca8a68ada99be5957d9aa5d374a4e32
erikvaningen <erikvaningen@gmail.com>
2013-12-04 14:47:26 +0100

    updating the clean script for the vme DB

--- vme-sql/dropVmeTables.sql
+++ vme-sql/dropVmeTables.sql
@@ -2,18 +2,20 @@ drop table information_source_rfmo CASCADE CONSTRAINTS;
 drop table rfmo_information_source CASCADE CONSTRAINTS;
 drop table information_source_list CASCADE CONSTRAINTS;
 drop table information_source CASCADE CONSTRAINTS;
-drop table rfmo_history CASCADE CONSTRAINTS;
 drop table vme_geo_ref CASCADE CONSTRAINTS;
 drop table geo_ref CASCADE CONSTRAINTS;
-drop table history CASCADE CONSTRAINTS;
 drop table specific_measure CASCADE CONSTRAINTS;
 drop table general_measure CASCADE CONSTRAINTS;
 drop table vme_profile CASCADE CONSTRAINTS;
 drop table vme_specific_measure CASCADE CONSTRAINTS;
-drop table profile;
-drop table vme;
-drop table rfmo;
-
+drop table profile CASCADE CONSTRAINTS;
+drop table vme CASCADE CONSTRAINTS;
+drop table rfmo CASCADE CONSTRAINTS;
+drop table fishery_areas_history CASCADE CONSTRAINTS;
+drop table vmes_history CASCADE CONSTRAINTS;
+drop table rfmo_fishery_areas_history CASCADE CONSTRAINTS;
+drop table multilingual_string CASCADE CONSTRAINTS;
+drop table multilingualstring_stringmap CASCADE CONSTRAINTS;
 
 --SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'GENERAL_MEASURES';
 
@@ -23,5 +25,6 @@ drop table rfmo;
 
 --SELECT table_name FROM USER_CONSTRAINTS
 --where r_owner = 'VME_ADMIN'
+
 
 
