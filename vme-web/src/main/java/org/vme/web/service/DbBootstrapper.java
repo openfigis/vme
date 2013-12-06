e83543e7ae2541716dc580d7f29e95269f70be32
erikvaningen <erikvaningen@gmail.com>
2013-12-06 16:41:47 +0100

    preparing vme-web for the new MS-Access DB

--- vme-web/src/main/java/org/vme/web/service/DbBootstrapper.java
+++ vme-web/src/main/java/org/vme/web/service/DbBootstrapper.java
@@ -3,56 +3,39 @@
  */
 package org.vme.web.service;
 
-import javax.inject.Inject;
-
 import org.fao.fi.vme.msaccess.VmeAccessDbImport;
 
 /**
  * @author SIBENI
- *
+ * 
  */
 public class DbBootstrapper {
-	
-	
+
 	/**
 	 * Microsoft access database file
 	 */
-	public final static int EMBEDDED_ACCESS_DB = 0 ;
-	
+	public final static int EMBEDDED_ACCESS_DB = 0;
+
 	private static boolean hasbooted = false;
-	
-	
 
-
-
-
-
-	@Inject
+	// @Inject
 	private VmeAccessDbImport vmeAccessDbImport;
-	
-	
-	
-	
+
 	public DbBootstrapper() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-
-
-
 	public void bootDb() throws Exception {
 		vmeAccessDbImport.importMsAccessData();
 		hasbooted = true;
 	}
-	
-	
+
 	/**
 	 * @return the hasbooted
 	 */
 	public static boolean hasbooted() {
 		return hasbooted;
-	}	
-	
-	
+	}
+
 }
