9aac9d5c7fc3e4e4e21bd9383108994aa4a40c44
erikvaningen <erikvaningen@gmail.com>
2014-01-15 11:32:50 +0100

    updating list of tables.

--- vme-sql/dropVmeTables.sql
+++ vme-sql/dropVmeTables.sql
@@ -1,21 +1,21 @@
-drop table information_source_rfmo CASCADE CONSTRAINTS;
-drop table rfmo_information_source CASCADE CONSTRAINTS;
-drop table information_source_list CASCADE CONSTRAINTS;
 drop table information_source CASCADE CONSTRAINTS;
 drop table vme_geo_ref CASCADE CONSTRAINTS;
 drop table geo_ref CASCADE CONSTRAINTS;
 drop table specific_measure CASCADE CONSTRAINTS;
 drop table general_measure CASCADE CONSTRAINTS;
-drop table vme_profile CASCADE CONSTRAINTS;
-drop table vme_specific_measure CASCADE CONSTRAINTS;
 drop table profile CASCADE CONSTRAINTS;
 drop table vme CASCADE CONSTRAINTS;
 drop table rfmo CASCADE CONSTRAINTS;
 drop table fishery_areas_history CASCADE CONSTRAINTS;
-drop table vmes_history CASCADE CONSTRAINTS;
-drop table rfmo_fishery_areas_history CASCADE CONSTRAINTS;
 drop table multilingual_string CASCADE CONSTRAINTS;
 drop table multilingualstring_stringmap CASCADE CONSTRAINTS;
+drop table vme_specific_measure CASCADE CONSTRAINTS;
+drop table vmes_history CASCADE CONSTRAINTS;
+
+
+
+
+
 
 --SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'GENERAL_MEASURES';
 
