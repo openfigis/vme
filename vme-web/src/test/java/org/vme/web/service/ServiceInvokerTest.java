54c50f6f5f5795aa3616754c35c3056d2fa8d9aa
erikvaningen <erikvaningen@gmail.com>
2014-01-22 14:51:42 +0100

    adding unit tests

--- vme-web/src/test/java/org/vme/web/service/ServiceInvokerTest.java
+++ vme-web/src/test/java/org/vme/web/service/ServiceInvokerTest.java
@@ -2,30 +2,38 @@ package org.vme.web.service;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.UUID;
+
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
 
 import org.jglue.cdiunit.ActivatedAlternatives;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.vme.service.dao.JpaDaoFactory;
+import org.vme.service.dao.ObservationDAO;
 import org.vme.service.dao.config.figis.FigisDataBaseProducer;
 import org.vme.service.dao.config.vme.VmeDataBaseProducer;
+import org.vme.web.service.io.ObservationsRequest;
+import org.vme.web.service.io.ServiceResponse;
 
 @RunWith(CdiRunner.class)
 @ActivatedAlternatives({ FigisDataBaseProducer.class, VmeDataBaseProducer.class, JpaDaoFactory.class })
-public class VmeSearchWsTest {
+public class ServiceInvokerTest {
 
 	@Inject
-	VmeSearchWs vmeSearchWs;
+	ObservationDAO service;
 
 	@Test
-	public void testFind() throws Exception {
-
-		String text = "lophelia";
-		Response response = vmeSearchWs.find(text, null, null, null, null);
+	public void testInvokeObservationDAOObservationsRequest() throws Exception {
+		ObservationsRequest request = new ObservationsRequest(UUID.randomUUID());
+		ServiceResponse<?> response = ServiceInvoker.invoke(service, request);
 		assertNotNull(response);
+	}
+
+	@Test
+	public void testInvokeReferenceDAOReferencesRequest() {
+		// fail("Not yet implemented");
 	}
 
 }
