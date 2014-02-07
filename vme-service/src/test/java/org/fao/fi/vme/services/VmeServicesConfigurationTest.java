fbe8b591e2b156b7d9fc9d6df98c5d8778b1f397
erikvaningen <erikvaningen@gmail.com>
2014-02-07 16:51:48 +0100

    Working with Maven classifier in order to have only 1 persistence.xml in the project, to be manipulated with Maven profiles

---
+++ vme-service/src/test/java/org/fao/fi/vme/services/VmeServicesConfigurationTest.java
@@ -0,0 +1,30 @@
+package org.fao.fi.vme.services;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+public class VmeServicesConfigurationTest {
+
+	@Test
+	public void testVmeConfig() {
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("META-INF/persistence.xml");
+		try {
+			assertNotNull("No persistence.xml found! Configure it through a Maven Profile", inputStream);
+			String text = IOUtils.toString(inputStream, "UTF-8");
+			System.out.println(text);
+			assertFalse(text.contains("oracle.jdbc.driver.OracleDriver"));
+			assertTrue("Expecting only to work with and find H2 here for testing", text.contains("org.h2.Driver"));
+		} catch (IOException e) {
+			fail();
+		}
+	}
+
+}
