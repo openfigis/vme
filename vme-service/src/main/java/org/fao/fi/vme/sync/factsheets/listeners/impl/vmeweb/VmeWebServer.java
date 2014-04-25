f93a762afee088f9a57462a28e234af5768fdc59
erikvaningen <erikvaningen@gmail.com>
2014-04-25 14:35:19 +0200

    adding comments

--- vme-service/src/main/java/org/fao/fi/vme/sync/factsheets/listeners/impl/vmeweb/VmeWebServer.java
+++ vme-service/src/main/java/org/fao/fi/vme/sync/factsheets/listeners/impl/vmeweb/VmeWebServer.java
@@ -1,5 +1,15 @@
 package org.fao.fi.vme.sync.factsheets.listeners.impl.vmeweb;
 
+/**
+ * This class represents the vme-web application, deployed for qa in
+ * http://hqldvfigis1:6010/figis/ws/vme/, not yet deployed in production.
+ * 
+ * 
+ * 
+ * 
+ * @author Erik van Ingen
+ * 
+ */
 public class VmeWebServer {
 	private String server;
 
