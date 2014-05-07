4de232998fd34bd6ec235520e4786679dcfc958f
empiriroberto <empiriroberto@gmail.com>
2014-05-07 10:41:57 +0200

    other fix for #1442 issue

--- vme-domain/src/main/java/org/fao/fi/vme/domain/model/InformationSource.java
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/model/InformationSource.java
@@ -265,78 +265,113 @@ public class InformationSource implements ObjectId<Long>, ReferenceReport {
 	 */
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
+		
 		InformationSource other = (InformationSource) obj;
+		
 		if (this.citation == null) {
-			if (other.citation != null)
+			if (other.citation != null) {
 				return false;
-		} else if (!this.citation.equals(other.citation))
+			}
+		} else if (!this.citation.equals(other.citation)) {
 			return false;
+		}
+		
 		if (this.sourceType == null) {
-			if (other.sourceType != null)
+			if (other.sourceType != null) {
 				return false;
-		} else if (!this.sourceType.equals(other.sourceType))
+			}
+		} else if (!this.sourceType.equals(other.sourceType)) {
 			return false;
+		}
 		if (this.committee == null) {
-			if (other.committee != null)
+			if (other.committee != null) {
 				return false;
-		} else if (!this.committee.equals(other.committee))
+			}
+		} else if (!this.committee.equals(other.committee)) {
 			return false;
+		}
 		if (this.generalMeasureList == null) {
-			if (other.generalMeasureList != null)
+			if (other.generalMeasureList != null) {
 				return false;
-		} else if (!this.generalMeasureList.equals(other.generalMeasureList))
+			}
+		} else if (!this.generalMeasureList.equals(other.generalMeasureList)) {
 			return false;
+		}
 		if (this.id == null) {
-			if (other.id != null)
+			if (other.id != null) {
 				return false;
-		} else if (!this.id.equals(other.id))
+			}
+		} else if (!this.id.equals(other.id)) {
 			return false;
+		}
 		if (this.meetingEndDate == null) {
-			if (other.meetingEndDate != null)
+			if (other.meetingEndDate != null) {
 				return false;
-		} else if (!this.meetingEndDate.equals(other.meetingEndDate))
+			}
+		} else if (!this.meetingEndDate.equals(other.meetingEndDate)) {
 			return false;
+		}
 		if (this.meetingStartDate == null) {
-			if (other.meetingStartDate != null)
+			if (other.meetingStartDate != null) {
 				return false;
-		} else if (!this.meetingStartDate.equals(other.meetingStartDate))
+			}
+		} else if (!this.meetingStartDate.equals(other.meetingStartDate)) {
 			return false;
+		}
 		if (this.publicationYear == null) {
-			if (other.publicationYear != null)
+			if (other.publicationYear != null) {
 				return false;
-		} else if (!this.publicationYear.equals(other.publicationYear))
+			}
+		} else if (!this.publicationYear.equals(other.publicationYear)) {
 			return false;
+		}
 		if (this.reportSummary == null) {
-			if (other.reportSummary != null)
+			if (other.reportSummary != null) {
 				return false;
-		} else if (!this.reportSummary.equals(other.reportSummary))
+			}
+		} else if (!this.reportSummary.equals(other.reportSummary)) {
 			return false;
+		}
 		if (this.rfmo == null) {
-			if (other.rfmo != null)
+			if (other.rfmo != null) {
 				return false;
-		} else if (!this.rfmo.equals(other.rfmo))
+			}
+		} else if (!this.rfmo.equals(other.rfmo)) {
 			return false;
+		}
 		if (this.sourceType == null) {
-			if (other.sourceType != null)
+			if (other.sourceType != null) {
 				return false;
-		} else if (!this.sourceType.equals(other.sourceType))
+			}
+		} else if (!this.sourceType.equals(other.sourceType)) {
 			return false;
+		}
 		if (this.specificMeasureList == null) {
-			if (other.specificMeasureList != null)
+			if (other.specificMeasureList != null) {
 				return false;
-		} else if (!this.specificMeasureList.equals(other.specificMeasureList))
+			}
+		} else if (!this.specificMeasureList.equals(other.specificMeasureList)) {
 			return false;
+		}
 		if (this.url == null) {
-			if (other.url != null)
+			if (other.url != null) {
 				return false;
-		} else if (!this.url.toString().equals(other.url.toString()))
+			}
+		} else if (!this.url.toString().equals(other.url.toString())) {
 			return false;
+		}
 		return true;
 	}
 }
