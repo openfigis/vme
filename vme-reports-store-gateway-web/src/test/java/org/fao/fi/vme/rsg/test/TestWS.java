4841d2d08ed6c55ff3ad1cc9d7574511bbc0b4ed
flowerside <flowerside@gmail.com>
2014-05-15 14:06:51 +0200

    Adding a local test (to be excluded from maven build)

---
+++ vme-reports-store-gateway-web/src/test/java/org/fao/fi/vme/rsg/test/TestWS.java
@@ -0,0 +1,37 @@
+/**
+ * (c) 2014 FAO / UN (project: vme-reports-store-gateway-web)
+ */
+package org.fao.fi.vme.rsg.test;
+
+import org.gcube.application.rsg.client.RsgClient;
+import org.gcube.application.rsg.service.dto.NameValue;
+import org.gcube.application.rsg.service.dto.ReportEntry;
+import org.gcube.application.rsg.service.dto.ReportType;
+import org.junit.Test;
+
+/**
+ * Place your class / interface description here.
+ *
+ * History:
+ *
+ * ------------- --------------- -----------------------
+ * Date			 Author			 Comment
+ * ------------- --------------- -----------------------
+ * 14 May 2014   Fiorellato     Creation.
+ *
+ * @version 1.0
+ * @since 14 May 2014
+ */
+public class TestWS {
+	@Test
+	public void testList() throws Throwable {
+		RsgClient client = new RsgClient("http://figisapps.fao.org/figis/secure-reports-store-gateway/rest/");
+		client.securedWithEncryptedToken("http://figisapps.fao.org/figis/secure-reports-store-gateway/security/token/encrypted/request", 
+										 "http://dev.d4science.org/pgp-security/imarine.skr", 
+										 "d45c13nc3", 
+										 "classpath://pgp/keys/owned/vme.pkr");
+		
+		for(ReportEntry in : client.listReports(new ReportType("Vme"), (NameValue[])null))
+			System.out.println(in.getId() + " : " + in.getIdentifier() + " : " + in.getOwner() + " : " + in.getReportType().getTypeIdentifier());
+	}
+}
