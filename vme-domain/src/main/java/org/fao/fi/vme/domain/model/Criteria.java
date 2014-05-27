affc256b2ef1b6bdc838972b65077150108ab27a
erikvaningen <erikvaningen@gmail.com>
2014-05-27 18:01:29 +0200

    adding the Criteria to the persistence.xml.

--- vme-domain/src/main/java/org/fao/fi/vme/domain/model/Criteria.java
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/model/Criteria.java
@@ -2,8 +2,10 @@ package org.fao.fi.vme.domain.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import org.gcube.application.rsg.support.compiler.bridge.annotations.RSGReferenceReport;
 import org.gcube.application.rsg.support.compiler.bridge.annotations.fields.RSGConverter;
@@ -33,6 +35,20 @@ public class Criteria implements ObjectId<Long>, Serializable {
 	@RSGConverter(LongDataConverter.class)
 	@Id
 	private Long id;
+
+	/**
+	 * 
+	 */
+	@OneToOne(cascade = CascadeType.MERGE)
+	private Vme vme;
+
+	public Vme getVme() {
+		return vme;
+	}
+
+	public void setVme(Vme vme) {
+		this.vme = vme;
+	}
 
 	public Long getId() {
 		return id;
