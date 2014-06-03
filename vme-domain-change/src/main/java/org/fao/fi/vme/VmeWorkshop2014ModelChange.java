9b37e3c03ffab84485ababcdeb218c3c245f21da
erikvaningen <erikvaningen@gmail.com>
2014-06-03 11:50:33 +0200

    Some objects do have empty validity periods

--- vme-domain-change/src/main/java/org/fao/fi/vme/VmeWorkshop2014ModelChange.java
+++ vme-domain-change/src/main/java/org/fao/fi/vme/VmeWorkshop2014ModelChange.java
@@ -11,8 +11,18 @@ public class VmeWorkshop2014ModelChange {
 	private ValidityPeriodMigrationElement validityPeriodMigrationElement;
 
 	public void migrate() {
+		System.out.println("VmeWorkshop2014ModelChange - start");
+
+		System.out.println("VmeWorkshop2014ModelChange - start criteriaMigrationElement");
 		criteriaMigrationElement.migrate();
+		System.out.println("VmeWorkshop2014ModelChange - done criteriaMigrationElement");
+
+		System.out.println("VmeWorkshop2014ModelChange - start validityPeriodMigrationElement");
 		validityPeriodMigrationElement.migrate();
+		System.out.println("VmeWorkshop2014ModelChange - done validityPeriodMigrationElement");
+
+		System.out.println("VmeWorkshop2014ModelChange - done");
+
 	}
 
 }
