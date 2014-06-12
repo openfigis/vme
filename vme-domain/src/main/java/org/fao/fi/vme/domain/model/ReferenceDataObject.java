6b808761cd547fd37236a8df04d497fdf12bd6fb
erikvaningen <erikvaningen@gmail.com>
2014-06-12 11:08:32 +0200

    adding comments

--- vme-domain/src/main/java/org/fao/fi/vme/domain/model/ReferenceDataObject.java
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/model/ReferenceDataObject.java
@@ -1,5 +1,17 @@
 package org.fao.fi.vme.domain.model;
 
+/**
+ * 
+ * The VME model has business and reference data objects. This interface helps
+ * to distinguish them more explicit.
+ * 
+ * 
+ * 
+ * @author Erik van Ingen
+ * 
+ * @param <T>
+ */
+
 public interface ReferenceDataObject<T> {
 
 	public T getId();
