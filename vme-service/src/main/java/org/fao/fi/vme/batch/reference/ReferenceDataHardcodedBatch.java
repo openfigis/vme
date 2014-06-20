eda28c26934255d166daaa9ea86d1b9af17436ad
flowerside <flowerside@gmail.com>
2014-06-20 11:01:16 +0200

    Changing the Area Types (as requested by A. Gentile on June 20th 2014).
    Area types are now:
    
    • Registered VME (CCAMLR) ["code": "VME"]
    • Risk Area (CCAMLR) ["code": "AREA_RISK"]
    • Fisheries Restricted Area (GFCM) ["code": "AREA_RESTRICTED"]
    • Seamount closure (NAFO) ["code": "CLOS_SEAM"]
    • Coral closure (NAFO) ["code": "CLOS_CORAL"]
    • Area of higher sponge and coral concentration (NAFO) ["code":
    "AREA_SPOCOR"]
    • VME closure (NEAFC) ["code": "CLOS_VME"]
    • Closed area (SEAFO) ["code": "CLOS_AREA"]
    • Area closed to bottom fishing (SPRFMO) ["code": "CLOS_AREA_BTMFISH"]
    
    Numeric IDs have been assigned (almost) randomly.

--- vme-service/src/main/java/org/fao/fi/vme/batch/reference/ReferenceDataHardcodedBatch.java
+++ vme-service/src/main/java/org/fao/fi/vme/batch/reference/ReferenceDataHardcodedBatch.java
@@ -42,8 +42,8 @@ public class ReferenceDataHardcodedBatch {
 	}
 
 	private void createVmeScopes() {
-		dao.syncStoreObject(new VmeScope(10l, "VME"));
-		dao.syncStoreObject(new VmeScope(20l, "Regulatory"));
+		dao.syncStoreObject(new VmeScope(10L, "VME"));
+		dao.syncStoreObject(new VmeScope(20L, "Regulatory"));
 	}
 
 	/**
@@ -89,79 +89,53 @@ public class ReferenceDataHardcodedBatch {
 
 	private void createAuthorities() {
 
-		dao.syncStoreObject(new Authority(20010l, "CCAMLR",
+		dao.syncStoreObject(new Authority(20010L, "CCAMLR",
 				"Commission for the Conservation of Antarctic Marine Living Resources"));
-		dao.syncStoreObject(new Authority(24561l, "GFCM", "General Fishery Commission for the Mediterranean sea"));
-		dao.syncStoreObject(new Authority(20220l, "NAFO", "Northwest Atlantic Fisheries Organization"));
-		dao.syncStoreObject(new Authority(21580l, "NEAFC", "North East Atlantic Fisheries Commission"));
-		dao.syncStoreObject(new Authority(22140l, "SEAFO", "South East Atlantic Fisheries Organisation"));
-		dao.syncStoreObject(new Authority(22150l, "WECAFC", "Western Central Atlantic Fishery Commission"));
-		dao.syncStoreObject(new Authority(24558l, "SPRFMO", "South Pacific Regional Fisheries Management Organisation"));
+		dao.syncStoreObject(new Authority(24561L, "GFCM", "General Fishery Commission for the Mediterranean sea"));
+		dao.syncStoreObject(new Authority(20220L, "NAFO", "Northwest Atlantic Fisheries Organization"));
+		dao.syncStoreObject(new Authority(21580L, "NEAFC", "North East Atlantic Fisheries Commission"));
+		dao.syncStoreObject(new Authority(22140L, "SEAFO", "South East Atlantic Fisheries Organisation"));
+		dao.syncStoreObject(new Authority(22150L, "WECAFC", "Western Central Atlantic Fishery Commission"));
+		dao.syncStoreObject(new Authority(24558L, "SPRFMO", "South Pacific Regional Fisheries Management Organisation"));
 
 		// repAuthority.put((long)90010, new
 		// Authority(22140,"SIODFA","Southern Indian Ocean Deepsea Fishers' Association");
 	}
 
 	private void createVmeCriterias() {
-		dao.syncStoreObject(new VmeCriteria(10l, "Uniqueness or rarity"));
-		dao.syncStoreObject(new VmeCriteria(20l, "Functional significance of the habitat"));
-		dao.syncStoreObject(new VmeCriteria(30l, "Fragility"));
-		dao.syncStoreObject(new VmeCriteria(40l, "Life-history traits"));
-		dao.syncStoreObject(new VmeCriteria(50l, "Structural complexity"));
-		dao.syncStoreObject(new VmeCriteria(60l, "Unspecified"));
+		dao.syncStoreObject(new VmeCriteria(10L, "Uniqueness or rarity"));
+		dao.syncStoreObject(new VmeCriteria(20L, "Functional significance of the habitat"));
+		dao.syncStoreObject(new VmeCriteria(30L, "Fragility"));
+		dao.syncStoreObject(new VmeCriteria(40L, "Life-history traits"));
+		dao.syncStoreObject(new VmeCriteria(50L, "Structural complexity"));
+		dao.syncStoreObject(new VmeCriteria(60L, "Unspecified"));
 	}
 
 	private void createVmeTypes() {
-		// dao.syncStoreObject(new VmeType(10l, "VME"));
-		// dao.syncStoreObject(new VmeType(20l, "Risk area"));
-		// dao.syncStoreObject(new VmeType(30l,
+		// dao.syncStoreObject(new VmeType(10L, "VME"));
+		// dao.syncStoreObject(new VmeType(20L, "Risk area"));
+		// dao.syncStoreObject(new VmeType(30L,
 		// "Other types of closed/restricted area"));
 
-		// these were the original ones
-		dao.syncStoreObject(new VmeType(10l, "VME", "Registered VMEs"));
-		dao.syncStoreObject(new VmeType(20l, "AREA_RISK", "Risk Areas"));
-		dao.syncStoreObject(new VmeType(30l, "CLOS_AREA", "Closed areas"));
-
-		// then these others were added on 12 Jun 2014, as a restult from the
-		// input of the VME 2014 Rome workshop.
-		dao.syncStoreObject(new VmeType(130l, "TTH_FISH_EST", "Established toothfish fisheries "));
-		dao.syncStoreObject(new VmeType(140l, "TTH_FISH_EXPL", "Exploratory toothfish fisheries"));
-		dao.syncStoreObject(new VmeType(150l, "TTH_FISH_RSCH", "Research fisheries"));
-		dao.syncStoreObject(new VmeType(160l, "BTM_FISH_FP", "Existing bottom fishing (footprint) areas "));
-		dao.syncStoreObject(new VmeType(170l, "BTM_FISH", "Existing bottom fishing areas"));
-		dao.syncStoreObject(new VmeType(180l, "BTM_FISH_SEAM", "Seamount fishing areas"));
-		dao.syncStoreObject(new VmeType(190l, "BTM_FISH", "Existing bottom fishing areas"));
-		dao.syncStoreObject(new VmeType(200l, "BTM_FISH_LL", "Existing bottom fishing areas (set longlines)"));
-		dao.syncStoreObject(new VmeType(210l, "BTM_FISH_FP_2", "Bottom fishing footprint"));
-
-		dao.syncStoreObject(new VmeType(240l, "AREA_RESTRICTED", "Fisheries Restricted Areas"));
-		dao.syncStoreObject(new VmeType(250l, "CLOS_SEAM", "Seamount closure"));
-		dao.syncStoreObject(new VmeType(260l, "CLOS_CORAL", "Coral closure"));
-		dao.syncStoreObject(new VmeType(270l, "AREA_SPOCOR", "Area of higher sponge and coral concentration"));
-		dao.syncStoreObject(new VmeType(280l, "CLOS_VME", "VME closure"));
-
-		dao.syncStoreObject(new VmeType(300l, "CLOS_AREA_BTMFISH", "Area closed to bottom fishing"));
-		dao.syncStoreObject(new VmeType(310l, "TTH_FISH_CLOS", "Closed toothfish fisheries"));
-		dao.syncStoreObject(new VmeType(320l, "AREA_SHALLOW", "Shallow water management"));
-		dao.syncStoreObject(new VmeType(330l, "AREA_DEEP", "Deepwater Fisheries Restriction"));
-		dao.syncStoreObject(new VmeType(340l, "BTM_FISH_NEW", "New bottom fishing areas"));
-		dao.syncStoreObject(new VmeType(350l, "BTM_UNFISHED", "Unfished bottom areas"));
-		dao.syncStoreObject(new VmeType(360l, "BTM_FISH_NEW", "New bottom fishing areas"));
-		dao.syncStoreObject(new VmeType(370l, "BTM_FISH_CLOS", "Prohibited bottom fishing  (provisional) areas "));
-		dao.syncStoreObject(new VmeType(380l, "AREA_ASSESS", "Areas requiring assessment"));
-		dao.syncStoreObject(new VmeType(390l, "BTM_FISH_NEW", "New bottom fishing areas"));
-		dao.syncStoreObject(new VmeType(400l, "BTM_FISH_OUT_FP", "Bottom fishing outside the footprint"));
-
+		//Changing VME Types (following a request by A. Gentile - June 20th 2014
+		dao.syncStoreObject(new VmeType(10L, "CLOS_AREA_BTMFISH", "Area closed to bottom fishing (SPRFMO)"));
+		dao.syncStoreObject(new VmeType(20L, "CLOS_VME", "VME closure (NEAFC)"));
+		dao.syncStoreObject(new VmeType(30L, "CLOS_AREA", "Closed area (SEAFO)"));
+		dao.syncStoreObject(new VmeType(40L, "AREA_SPOCOR", "Area of higher sponge and coral concentration (NAFO)"));
+		dao.syncStoreObject(new VmeType(41L, "CLOS_CORAL", "Coral closure (NAFO)"));
+		dao.syncStoreObject(new VmeType(42L, "CLOS_SEAM", "Seamount closure (NAFO)"));
+		dao.syncStoreObject(new VmeType(50L, "AREA_RESTRICTED", "Fisheries Restricted Area (GFCM)"));
+		dao.syncStoreObject(new VmeType(60L, "AREA_RISK", "Risk Area (CCAMLR)"));
+		dao.syncStoreObject(new VmeType(61L, "VME", "Registered VME (CCAMLR)"));
 	}
 
 	private void createInformationSourceTypes() {
-		dao.syncStoreObject(new InformationSourceType(1l, "Book", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
-		dao.syncStoreObject(new InformationSourceType(2l, "Meeting documents",
-				InformationSourceType.IS_A_MEETING_DOCUMENT));
-		dao.syncStoreObject(new InformationSourceType(3l, "Journal", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
-		dao.syncStoreObject(new InformationSourceType(4l, "Project", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
-		dao.syncStoreObject(new InformationSourceType(6l, "CD-ROM/DVD", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
-		dao.syncStoreObject(new InformationSourceType(99l, "Other", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(1L, "Book", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(2L, "Meeting documents", InformationSourceType.IS_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(3L, "Journal", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(4L, "Project", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(6L, "CD-ROM/DVD", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
+		dao.syncStoreObject(new InformationSourceType(99L, "Other", InformationSourceType.IS_NOT_A_MEETING_DOCUMENT));
 	}
 
 	// private void createYears() {
