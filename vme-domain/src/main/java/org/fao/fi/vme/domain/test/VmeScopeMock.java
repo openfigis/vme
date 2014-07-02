ee4564c637be0af40f28d4185793d09c1a325169
erikvaningen <erikvaningen@gmail.com>
2014-07-02 17:03:31 +0200

    adding missing class

---
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/test/VmeScopeMock.java
@@ -0,0 +1,16 @@
+package org.fao.fi.vme.domain.test;
+
+import org.fao.fi.vme.domain.model.reference.VmeScope;
+
+public class VmeScopeMock {
+
+	public static VmeScope create() {
+		VmeScope vmeScope = new VmeScope();
+		vmeScope.setId(VmeMock.VME_SCOPE);
+		vmeScope.setCodeSystem("vme");
+		vmeScope.setName("VME");
+
+		return vmeScope;
+	}
+
+}
