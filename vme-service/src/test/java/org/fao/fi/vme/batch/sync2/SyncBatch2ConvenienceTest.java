2644cd10067272bcb9d29bf7a620c9bd4691432c
erikvaningen <erikvaningen@gmail.com>
2014-07-08 11:58:03 +0200

    adding test

---
+++ vme-service/src/test/java/org/fao/fi/vme/batch/sync2/SyncBatch2ConvenienceTest.java
@@ -0,0 +1,40 @@
+package org.fao.fi.vme.batch.sync2;
+
+import javax.inject.Inject;
+
+import org.jglue.cdiunit.ActivatedAlternatives;
+import org.jglue.cdiunit.AdditionalClasses;
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.vme.dao.config.figis.FigisDataBaseProducer;
+import org.vme.dao.config.figis.FigisPersistenceUnitConfiguration;
+import org.vme.dao.config.vme.VmeDataBaseProducerApplicationScope;
+import org.vme.dao.config.vme.VmePersistenceUnitConfiguration;
+import org.vme.dao.impl.jpa.ReferenceDaoImpl;
+
+/**
+ * This test is for convenience purposese in order to run the batch. In this
+ * case the maven profile on the vme-configuration needs to be specified with
+ * the correct environment, for instance vme=fiqa.
+ * 
+ * 
+ * @author Erik van Ingen
+ * 
+ */
+@RunWith(CdiRunner.class)
+@AdditionalClasses({ ReferenceDaoImpl.class })
+@ActivatedAlternatives({ VmePersistenceUnitConfiguration.class, VmeDataBaseProducerApplicationScope.class,
+		FigisDataBaseProducer.class, FigisPersistenceUnitConfiguration.class })
+public class SyncBatch2ConvenienceTest {
+
+	@Inject
+	private SyncBatch2 syncBatch2;
+
+	@Test
+	public void testSyncFigisWithVmePrimaryRule() {
+
+		syncBatch2.syncFigisWithVme();
+
+	}
+}
