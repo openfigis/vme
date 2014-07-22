a314181150fb5a0e193256097f94cad033323500
erikvaningen <erikvaningen@gmail.com>
2014-07-22 17:20:40 +0200

    extending the mock

--- vme-web-dto-jaxb/src/main/java/org/fao/fi/vme/dto/test/SpecificMeasureTypeMock.java
+++ vme-web-dto-jaxb/src/main/java/org/fao/fi/vme/dto/test/SpecificMeasureTypeMock.java
@@ -9,14 +9,19 @@ import org.fao.fi.vme.webservice.SpecificMeasureType;
 
 public class SpecificMeasureTypeMock {
 
-	private static int ID = 10;
+	private static final int ID = 10;
+	private static int OID = 958;
 
 	public static SpecificMeasureType create() {
 		SpecificMeasureType s1 = new SpecificMeasureType();
-		s1.setId(ID++);
+		s1.setOid(OID++);
+		s1.setId(ID);
 		s1.setYear(1985);
 		s1.setLang("en");
 		s1.setMeasureSourceUrl("http://archive.nafo.int/open/fc/2013/fcdoc13-01.pdf");
+		String text = "Text met <b>kippesoep<b> en aardbeien. ";
+		String cddataText = "<![CDATA[" + text + "]]>";
+		s1.setMeasureText(cddataText);
 
 		GregorianCalendar gcal = new GregorianCalendar();
 		try {
@@ -29,5 +34,4 @@ public class SpecificMeasureTypeMock {
 		return s1;
 
 	}
-
 }
