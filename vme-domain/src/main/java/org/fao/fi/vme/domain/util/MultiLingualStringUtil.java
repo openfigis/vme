5cfd92a69c7a65e44647ec5a68edaaf4045345de
erikvaningen <erikvaningen@gmail.com>
2014-09-04 11:44:59 +0200

    adding support for JPA for MultiLingualString

--- vme-domain/src/main/java/org/fao/fi/vme/domain/util/MultiLingualStringUtil.java
+++ vme-domain/src/main/java/org/fao/fi/vme/domain/util/MultiLingualStringUtil.java
@@ -1,9 +1,12 @@
 package org.fao.fi.vme.domain.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.fao.fi.vme.VmeException;
 import org.fao.fi.vme.domain.model.MultiLingualString;
 
 /**
@@ -14,6 +17,13 @@ import org.fao.fi.vme.domain.model.MultiLingualString;
  */
 public class MultiLingualStringUtil {
 
+	// index of the word starting after the get of the set in the method name.
+	private static int XET = 3;
+	// index of the first parameter of the setter
+	private static int SET_INDEX = 0;
+	// number of parameters of a setter of a bean
+	private static int LENGTH = 1;
+
 	public MultiLingualString english(String text) {
 		MultiLingualString l = new MultiLingualString();
 		Map<Integer, String> stringMap = new HashMap<Integer, String>();
@@ -21,7 +31,7 @@ public class MultiLingualStringUtil {
 		l.setStringMap(stringMap);
 		return l;
 	}
-	
+
 	public void addFrench(MultiLingualString u, String text) {
 		u.getStringMap().put(Lang.FR, text);
 	}
@@ -43,4 +53,56 @@ public class MultiLingualStringUtil {
 		return english;
 	}
 
+	/**
+	 * Replace the English text with a new text.
+	 * 
+	 * The MultiLingualString needs to contain English text, in order to be able
+	 * to be replaced.
+	 * 
+	 * @param l
+	 * @param text
+	 */
+	public void replaceEnglish(MultiLingualString l, String text) {
+		if (l.getStringMap() == null || l.getStringMap().get(Lang.EN) == null) {
+			throw new VmeException(
+					"The MultiLingualString did not contain any text, it should contain it in order to be able to be replaced.");
+		}
+		l.getStringMap().put(Lang.EN, text);
+	}
+
+	/**
+	 * Utility for using JPA in combination with MultiLingualString. It copies
+	 * the values not the objects.
+	 * 
+	 * Be aware that this supports only the English language yet.
+	 * 
+	 * 
+	 * @param source
+	 * @param destination
+	 */
+	public void copyMultiLingual(Object source, Object destination) {
+		Method[] methods = source.getClass().getMethods();
+		for (Method getMethod : methods) {
+			if (getMethod.getReturnType().equals(MultiLingualString.class) && getMethod.getParameterTypes().length == 0) {
+				for (Method setMethod : methods) {
+					if (getMethod.getName().substring(XET).equals(setMethod.getName().substring(XET))
+							&& setMethod.getParameterTypes().length == LENGTH
+							&& setMethod.getParameterTypes()[SET_INDEX].equals(MultiLingualString.class)) {
+						try {
+							MultiLingualString s = (MultiLingualString) getMethod.invoke(source);
+							MultiLingualString d = (MultiLingualString) getMethod.invoke(destination);
+							if (s == null ^ d == null) {
+								setMethod.invoke(destination, s);
+							}
+							if (s != null && d != null) {
+								this.replaceEnglish(d, this.getEnglish(s));
+							}
+						} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+							throw new VmeException(e);
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- vme-domain/src/test/java/org/fao/fi/vme/domain/util/MultiLingualStringUtilTest.java
+++ vme-domain/src/test/java/org/fao/fi/vme/domain/util/MultiLingualStringUtilTest.java
@@ -2,7 +2,9 @@ package org.fao.fi.vme.domain.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
+import org.fao.fi.vme.domain.model.GeneralMeasure;
 import org.fao.fi.vme.domain.model.MultiLingualString;
 import org.junit.Test;
 
@@ -11,14 +13,103 @@ public class MultiLingualStringUtilTest {
 	MultiLingualStringUtil u = new MultiLingualStringUtil();
 
 	@Test
+	public void erik() {
+		System.out.println(0 ^ 0);
+		System.out.println(0 ^ 1);
+		System.out.println(1 ^ 0);
+		System.out.println(1 ^ 1);
+
+		System.out.println(0 | 0);
+		System.out.println(0 | 1);
+		System.out.println(1 | 0);
+		System.out.println(1 | 1);
+	}
+
+	@Test
+	public void testCopyMultiLingual() {
+		String s1 = "Mozart";
+		String s2 = "Beethoven";
+		MultiLingualString m1 = u.english(s1);
+		MultiLingualString m2 = u.english(s2);
+
+		GeneralMeasure source = new GeneralMeasure();
+		source.setFishingArea(m1);
+		GeneralMeasure destination = new GeneralMeasure();
+		destination.setFishingArea(m2);
+		Object oDestination = destination.getFishingArea().getStringMap();
+
+		assertEquals(u.getEnglish(source.getFishingArea()), s1);
+		assertEquals(u.getEnglish(destination.getFishingArea()), s2);
+		u.copyMultiLingual(source, destination);
+
+		assertEquals(u.getEnglish(source.getFishingArea()), s1);
+		assertEquals(u.getEnglish(destination.getFishingArea()), s1);
+
+		// should still contain only 1 string
+		assertEquals(1, destination.getFishingArea().getStringMap().size());
+
+		// make sure the destination has not changed object, only the string
+		// needs to be changed.
+		assertEquals(oDestination, destination.getFishingArea().getStringMap());
+
+	}
+
+	@Test
+	public void testCopyMultiLingualNullCaseDestination() {
+		String s1 = "Mozart";
+		MultiLingualString m1 = u.english(s1);
+		GeneralMeasure source = new GeneralMeasure();
+		source.setFishingArea(m1);
+		GeneralMeasure destination = new GeneralMeasure();
+		u.copyMultiLingual(source, destination);
+		assertEquals(s1, u.getEnglish(destination.getFishingArea()));
+	}
+
+	@Test
+	public void testCopyMultiLingualNullCaseSource() {
+		String s1 = "Mozart";
+		MultiLingualString m1 = u.english(s1);
+		GeneralMeasure source = new GeneralMeasure();
+		GeneralMeasure destination = new GeneralMeasure();
+		destination.setFishingArea(m1);
+		u.copyMultiLingual(source, destination);
+		assertEquals(null, u.getEnglish(destination.getFishingArea()));
+	}
+
+	@Test
+	public void testReplace() {
+		String text = "Een klachten vrije wereld";
+		MultiLingualString l = u.english(text);
+		assertEquals(text, l.getStringMap().get(Lang.EN));
+		Object o = l.getStringMap();
+
+		String newText = "Een vrije klachten wereld";
+		u.replaceEnglish(l, newText);
+		assertEquals(newText, l.getStringMap().get(Lang.EN));
+		assertEquals(o, l.getStringMap());
+
+	}
+
+	@Test
+	public void testReplaceEmpty() {
+		MultiLingualString l = new MultiLingualString();
+		String newText = "Een vrije klachten wereld";
+		try {
+			u.replaceEnglish(l, newText);
+			fail();
+		} catch (Exception e) {
+		}
+	}
+
+	@Test
 	public void testEnglish() {
 		String text = "Een klachten vrije wereld";
 		MultiLingualString l = u.english(text);
 		assertEquals(text, l.getStringMap().get(Lang.EN));
 	}
-	
+
 	@Test
-	public void testGetEnglish(){
+	public void testGetEnglish() {
 		MultiLingualString l = new MultiLingualString();
 		l = u.english("");
 		assertNull(u.getEnglish(l));
