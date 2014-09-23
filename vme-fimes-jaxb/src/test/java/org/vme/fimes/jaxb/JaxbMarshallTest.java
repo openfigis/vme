e6034be27f2a2d035d3bcc35297b14377c1f7a6b
erikvaningen <erikvaningen@gmail.com>
2014-09-23 13:57:59 +0200

    developing test to represent bug
    <fi:Min xsi:type="xs:string"
    xmlns:xs="http://www.w3.org/2001/XMLSchema">2000-01-01</fi:Min>

--- vme-fimes-jaxb/src/test/java/org/vme/fimes/jaxb/JaxbMarshallTest.java
+++ vme-fimes-jaxb/src/test/java/org/vme/fimes/jaxb/JaxbMarshallTest.java
@@ -1,17 +1,57 @@
 package org.vme.fimes.jaxb;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import javax.xml.bind.JAXBElement;
 
 import org.fao.fi.figis.devcon.FIGISDoc;
 import org.fao.fi.figis.devcon.GeneralBiology;
 import org.fao.fi.figis.devcon.HabitatBio;
+import org.fao.fi.figis.devcon.Min;
+import org.fao.fi.figis.devcon.ObjectFactory;
+import org.fao.fi.figis.devcon.Range;
 import org.fao.fi.figis.devcon.Text;
 import org.fao.fi.figis.devcon.VME;
+import org.fao.fi.figis.devcon.VMEIdent;
 import org.junit.Test;
 
 public class JaxbMarshallTest {
 
 	JaxbMarshall m = new JaxbMarshall();
+
+	@Test
+	public void testMarshalToStringMinMax() {
+
+		ObjectFactory f = new ObjectFactory();
+		JAXBElement<Min> minJAXBElement = f.createRangeMin(f.createMin());
+		fillObject("2010", minJAXBElement);
+
+		Range range = f.createRange();
+		range.setType("Time");
+		range.getContent().add(minJAXBElement);
+
+		VMEIdent vmeIdent = new VMEIdent();
+		vmeIdent.getFigisIDsAndForeignIDsAndWaterAreaReves().add(range);
+
+		VME vme = new VME();
+		vme.setVMEIdent(vmeIdent);
+		FIGISDoc doc = new FIGISDoc();
+		doc.setVME(vme);
+
+		String s = m.marshalToString(doc);
+		System.out.println(s);
+		assertFalse(s.contains("xsi:type=\"xs:string"));
+
+	}
+
+	public <T> void fillObject(Object value, JAXBElement<T> jaxbObject) {
+		if (value != null && jaxbObject != null) {
+			@SuppressWarnings("unchecked")
+			T cccc = (T) value.toString();
+			jaxbObject.setValue(cccc);
+		}
+	}
 
 	@Test
 	public void testMarshalToString() {
