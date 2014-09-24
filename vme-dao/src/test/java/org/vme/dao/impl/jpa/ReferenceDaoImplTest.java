82b0d1835a5a4aa161ded50fbc9dbb95ad5f78cb
erikvaningen <erikvaningen@gmail.com>
2014-09-24 14:04:18 +0200

    committed in the wrong branch, reverting

--- vme-dao/src/test/java/org/vme/dao/impl/jpa/ReferenceDaoImplTest.java
+++ vme-dao/src/test/java/org/vme/dao/impl/jpa/ReferenceDaoImplTest.java
@@ -28,7 +28,7 @@ public class ReferenceDaoImplTest {
 
 	@Test
 	public void testGetConcepts() throws ReferenceServiceException {
-		assertEquals(7, referenceDao.getConcepts().size());
+		assertEquals(6, referenceDao.getConcepts().size());
 	}
 
 	@Test
