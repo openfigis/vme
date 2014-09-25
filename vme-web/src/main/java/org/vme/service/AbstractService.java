bc3a911f01b1daefe452f9bdb430bb7f37ed48dc
erikvaningen <erikvaningen@gmail.com>
2014-09-25 11:38:02 +0200

    fixing bug

--- vme-web/src/main/java/org/vme/service/AbstractService.java
+++ vme-web/src/main/java/org/vme/service/AbstractService.java
@@ -36,7 +36,7 @@ abstract class AbstractService {
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractService.class);
 	private static final String UNABLE2RETRIVE = "Unable to retrieve reference {} by ID {}: {}";
 
-	public List<Vme> filterVmePerRfmo(List<Vme> vmeList, String authorityAcronym){
+	public List<Vme> filterVmePerRfmo(List<Vme> vmeList, String authorityAcronym) {
 		List<Vme> vmeListPerRfmo = new ArrayList<Vme>();
 
 		for (Vme v : vmeList) {
@@ -47,7 +47,7 @@ abstract class AbstractService {
 		return vmeListPerRfmo;
 	}
 
-	public List<Vme> filterVmePerScope(List<Vme> vmeList, String scope){
+	public List<Vme> filterVmePerScope(List<Vme> vmeList, String scope) {
 		List<Vme> vmeListPerScope = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
 			try {
@@ -61,10 +61,10 @@ abstract class AbstractService {
 		return vmeListPerScope;
 	}
 
-	public void filterVmePerId(List<Vme> vmeList, Long vmeId){
+	public void filterVmePerId(List<Vme> vmeList, Long vmeId) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
-			if(vmeId.equals(vme.getId())){
+			if (vmeId.equals(vme.getId())) {
 				temp.add(vme);
 			}
 		}
@@ -72,10 +72,10 @@ abstract class AbstractService {
 		vmeList.addAll(temp);
 	}
 
-	public void filterVmePerInventoryIdentifier(List<Vme> vmeList, String inventoryIdentifier){
+	public void filterVmePerInventoryIdentifier(List<Vme> vmeList, String inventoryIdentifier) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
-			if(vme.getInventoryIdentifier().equals(inventoryIdentifier)){
+			if (vme.getInventoryIdentifier().equals(inventoryIdentifier)) {
 				temp.add(vme);
 			}
 		}
@@ -86,7 +86,7 @@ abstract class AbstractService {
 	public void filterVmePerType(List<Vme> vmeList, int type) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
-			if(vme.getAreaType()!=null && vme.getAreaType().intValue() == type){
+			if (vme.getAreaType() != null && vme.getAreaType().intValue() == type) {
 				temp.add(vme);
 			}
 		}
@@ -97,7 +97,7 @@ abstract class AbstractService {
 	public void filterVmePerRfmoById(List<Vme> vmeList, int authority) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
-			if(getAuthorityIdByAcronym(vme.getRfmo().getId()) == authority){
+			if (getAuthorityIdByAcronym(vme.getRfmo().getId()) == authority) {
 				temp.add(vme);
 			}
 		}
@@ -108,7 +108,7 @@ abstract class AbstractService {
 	public void filterVmePerRelevantText(List<Vme> vmeList, String text) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
-			if(vmeContainRelevantText(vme, text)){
+			if (vmeContainRelevantText(vme, text)) {
 				temp.add(vme);
 			}
 		}
@@ -116,16 +116,16 @@ abstract class AbstractService {
 		vmeList.addAll(temp);
 	}
 
-	public void filterVmePerGeographFeatureId(List<Vme> vmeList, String geographicFeatureId){
+	public void filterVmePerGeographFeatureId(List<Vme> vmeList, String geographicFeatureId) {
 		List<Vme> temp = new ArrayList<Vme>();
 		for (Vme vme : vmeList) {
 			boolean found = false;
 			for (GeoRef gr : vme.getGeoRefList()) {
-				if(gr.getGeographicFeatureID().equals(geographicFeatureId)){
+				if (gr.getGeographicFeatureID().equals(geographicFeatureId)) {
 					found = true;
 				}
 			}
-			if (found){
+			if (found) {
 				temp.add(vme);
 			}
 		}
@@ -138,11 +138,11 @@ abstract class AbstractService {
 		for (Vme vme : vmeList) {
 			boolean found = false;
 			for (Long cr : vme.getCriteria()) {
-				if(cr == criteria){
+				if (cr == criteria) {
 					found = true;
 				}
 			}
-			if (found){
+			if (found) {
 				temp.add(vme);
 			}
 		}
@@ -152,19 +152,19 @@ abstract class AbstractService {
 
 	public void filterByRequestParam(List<Vme> vmeList, int authority, int type, int criteria, String text) {
 
-		if(authority > 0){
+		if (authority > 0) {
 			filterVmePerRfmoById(vmeList, authority);
 		}
 
-		if(type > 0){
+		if (type > 0) {
 			filterVmePerType(vmeList, type);
 		}
 
-		if(criteria > 0){
+		if (criteria > 0) {
 			filterVmePerCriteria(vmeList, criteria);
 		}
 
-		if(text != null){
+		if (text != null) {
 			filterVmePerRelevantText(vmeList, text);
 		}
 
@@ -192,9 +192,10 @@ abstract class AbstractService {
 
 	public boolean vmeContainRelevantText(Vme vme, String text) {
 
-		if(textInAreaType(vme, text) || textInCriteria(vme, text) || textInMultilingualString(vme.getGeoArea(), text) || textInGeoRef(vme, text) || 
-				textInInventoryIdentifier(vme, text) || textInMultilingualString(vme.getName(), text) || textInProfileList(vme, text) || 
-				textInRfmo(vme, text) || textInSpecificMeasureList(vme, text)){
+		if (textInAreaType(vme, text) || textInCriteria(vme, text) || textInMultilingualString(vme.getGeoArea(), text)
+				|| textInGeoRef(vme, text) || textInInventoryIdentifier(vme, text)
+				|| textInMultilingualString(vme.getName(), text) || textInProfileList(vme, text)
+				|| textInRfmo(vme, text) || textInSpecificMeasureList(vme, text)) {
 			return true;
 		} else {
 			return false;
@@ -206,25 +207,27 @@ abstract class AbstractService {
 		if (vme.getSpecificMeasureList() != null) {
 			for (SpecificMeasure specificMeasure : vme.getSpecificMeasureList()) {
 				if (specificMeasure.getVmeSpecificMeasure() != null) {
-					if(textInMultilingualString(specificMeasure.getVmeSpecificMeasure(), text)){
+					if (textInMultilingualString(specificMeasure.getVmeSpecificMeasure(), text)) {
 						return true;
 					}
 				}
 
 				if (specificMeasure.getInformationSource() != null) {
-					if(textInInformationSource(specificMeasure.getInformationSource(), text)){
+					if (textInInformationSource(specificMeasure.getInformationSource(), text)) {
 						return true;
 					}
 				}
 
-				if (specificMeasure.getInformationSource() != null && specificMeasure.getInformationSource().getCommittee() != null) {
-					if(textInMultilingualString(specificMeasure.getInformationSource().getCommittee(), text)){
+				if (specificMeasure.getInformationSource() != null
+						&& specificMeasure.getInformationSource().getCommittee() != null) {
+					if (textInMultilingualString(specificMeasure.getInformationSource().getCommittee(), text)) {
 						return true;
 					}
 				}
 
-				if (specificMeasure.getInformationSource() != null && specificMeasure.getInformationSource().getReportSummary() != null) {
-					if(textInMultilingualString(specificMeasure.getInformationSource().getReportSummary(), text)){
+				if (specificMeasure.getInformationSource() != null
+						&& specificMeasure.getInformationSource().getReportSummary() != null) {
+					if (textInMultilingualString(specificMeasure.getInformationSource().getReportSummary(), text)) {
 						return true;
 					}
 				}
@@ -233,10 +236,12 @@ abstract class AbstractService {
 		return false;
 	}
 
-	private boolean textInMultilingualString(MultiLingualString mString, String text){
-		for (String element : mString.getStringMap().values()) {
-			if (StringUtils.containsIgnoreCase(element, text)){
-				return true;
+	private boolean textInMultilingualString(MultiLingualString mString, String text) {
+		if (mString != null && mString.getStringMap() != null) {
+			for (String element : mString.getStringMap().values()) {
+				if (StringUtils.containsIgnoreCase(element, text)) {
+					return true;
+				}
 			}
 		}
 		return false;
@@ -244,15 +249,16 @@ abstract class AbstractService {
 
 	private boolean textInInformationSource(InformationSource informationSource, String text) {
 		if (informationSource.getCitation() != null) {
-			if(textInMultilingualString(informationSource.getCitation(), text)){
+			if (textInMultilingualString(informationSource.getCitation(), text)) {
 				return true;
 			}
 		}
-		if (informationSource.getPublicationYear() != null 
+		if (informationSource.getPublicationYear() != null
 				&& StringUtils.containsIgnoreCase(Integer.toString(informationSource.getPublicationYear()), text)) {
 			return true;
 		}
-		if (StringUtils.containsIgnoreCase(informationSource.getUrl() != null ? informationSource.getUrl().toExternalForm() : "", text)) {
+		if (StringUtils.containsIgnoreCase(informationSource.getUrl() != null ? informationSource.getUrl()
+				.toExternalForm() : "", text)) {
 			return true;
 		}
 		return false;
@@ -262,31 +268,31 @@ abstract class AbstractService {
 		if (vme.getRfmo() != null) {
 			for (GeneralMeasure generalMeasure : vme.getRfmo().getGeneralMeasureList()) {
 				if (generalMeasure.getFishingArea() != null) {
-					if(textInMultilingualString(generalMeasure.getFishingArea(), text)){
+					if (textInMultilingualString(generalMeasure.getFishingArea(), text)) {
 						return true;
 					}
 				}
 
 				if (generalMeasure.getExplorataryFishingProtocol() != null) {
-					if(textInMultilingualString(generalMeasure.getExplorataryFishingProtocol(), text)){
+					if (textInMultilingualString(generalMeasure.getExplorataryFishingProtocol(), text)) {
 						return true;
 					}
 				}
 
 				if (generalMeasure.getVmeEncounterProtocol() != null) {
-					if(textInMultilingualString(generalMeasure.getVmeEncounterProtocol(), text)){
+					if (textInMultilingualString(generalMeasure.getVmeEncounterProtocol(), text)) {
 						return true;
 					}
 				}
 
 				if (generalMeasure.getVmeIndicatorSpecies() != null) {
-					if(textInMultilingualString(generalMeasure.getVmeIndicatorSpecies(), text)){
+					if (textInMultilingualString(generalMeasure.getVmeIndicatorSpecies(), text)) {
 						return true;
 					}
 				}
 
 				if (generalMeasure.getVmeThreshold() != null) {
-					if(textInMultilingualString(generalMeasure.getVmeThreshold(), text)){
+					if (textInMultilingualString(generalMeasure.getVmeThreshold(), text)) {
 						return true;
 					}
 				}
@@ -295,18 +301,18 @@ abstract class AbstractService {
 					for (InformationSource informationSource : generalMeasure.getInformationSourceList()) {
 
 						if (informationSource.getCitation() != null) {
-							if(textInMultilingualString(informationSource.getCitation(), text)){
+							if (textInMultilingualString(informationSource.getCitation(), text)) {
 								return true;
 							}
 						}
 						if (informationSource.getCommittee() != null) {
-							if(textInMultilingualString(informationSource.getCommittee(), text)){
+							if (textInMultilingualString(informationSource.getCommittee(), text)) {
 								return true;
 							}
 						}
 
 						if (informationSource.getReportSummary() != null) {
-							if(textInMultilingualString(informationSource.getReportSummary(), text)){
+							if (textInMultilingualString(informationSource.getReportSummary(), text)) {
 								return true;
 							}
 						}
@@ -330,17 +336,17 @@ abstract class AbstractService {
 		if (vme.getProfileList() != null) {
 			for (Profile profile : vme.getProfileList()) {
 				if (profile.getDescriptionBiological() != null) {
-					if(textInMultilingualString(profile.getDescriptionBiological(), text)){
+					if (textInMultilingualString(profile.getDescriptionBiological(), text)) {
 						return true;
 					}
 				}
 				if (profile.getDescriptionImpact() != null) {
-					if(textInMultilingualString(profile.getDescriptionImpact(), text)){
+					if (textInMultilingualString(profile.getDescriptionImpact(), text)) {
 						return true;
 					}
 				}
 				if (profile.getDescriptionPhisical() != null) {
-					if(textInMultilingualString(profile.getDescriptionPhisical(), text)){
+					if (textInMultilingualString(profile.getDescriptionPhisical(), text)) {
 						return true;
 					}
 				}
@@ -366,15 +372,15 @@ abstract class AbstractService {
 	}
 
 	private boolean textInCriteria(Vme vme, String text) {
-		if(vme.getCriteria() != null && !vme.getCriteria().isEmpty()) {
+		if (vme.getCriteria() != null && !vme.getCriteria().isEmpty()) {
 			VmeCriteria selected = null;
 
-			for(Long criteriaId : vme.getCriteria()) {
+			for (Long criteriaId : vme.getCriteria()) {
 				try {
 					selected = refDao.getReferenceByID(VmeCriteria.class, criteriaId);
 
-					if(selected != null && selected.getName() != null) {
-						if(StringUtils.containsIgnoreCase(selected.getName(), text)) {
+					if (selected != null && selected.getName() != null) {
+						if (StringUtils.containsIgnoreCase(selected.getName(), text)) {
 							return true;
 						}
 					} else {
@@ -389,14 +395,14 @@ abstract class AbstractService {
 	}
 
 	private boolean textInAreaType(Vme vme, String text) {
-		if(vme.getAreaType() != null) {
+		if (vme.getAreaType() != null) {
 			VmeType selected = null;
 
 			try {
 				selected = refDao.getReferenceByID(VmeType.class, vme.getAreaType());
 
-				if(selected != null && selected.getName() != null) {
-					if(StringUtils.containsIgnoreCase(selected.getName(), text)) {
+				if (selected != null && selected.getName() != null) {
+					if (StringUtils.containsIgnoreCase(selected.getName(), text)) {
 						return true;
 					}
 				} else {
@@ -410,4 +416,3 @@ abstract class AbstractService {
 	}
 
 }
-
