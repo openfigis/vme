dff00a67d52d999b425f715819544909406ce0f7
erikvaningen <erikvaningen@gmail.com>
2014-10-06 17:47:51 +0200

    preparing cover pages

---
+++ vme-sql/rfmo-coverpage.sql
@@ -0,0 +1,37 @@
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (861,'CCAMLR Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (862,'GFCM Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (863,'NAFO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (864,'NEAFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (865,'NPFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (866,'SEAFO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (867,'SPRFMO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (868,'WECAFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+
+--  
+
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,861,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,862,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,863,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,864,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,865,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,866,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,867,'factsheet');
+Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
+values (7300,868,'factsheet');
+
+
