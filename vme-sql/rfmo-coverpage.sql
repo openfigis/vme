ef85d3ee10a2672a317fb9a6201d05d90d5240ad
erikvaningen <erikvaningen@gmail.com>
2014-10-07 12:27:36 +0200

    name changes

--- vme-sql/rfmo-coverpage.sql
+++ vme-sql/rfmo-coverpage.sql
@@ -1,21 +1,21 @@
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (861,'CCAMLR Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (862,'GFCM Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (863,'NAFO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (864,'NEAFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (865,'NPFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (866,'SEAFO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (867,'SPRFMO Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
-Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
-values (868,'WECAFC Conservation & Enforcement measures','IDENTIFIER','SUBJECT','FAO');
 
---  
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (861,'CCAMLR_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (862,'GFCM_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (863,'NAFO_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (864,'NEAFC_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (865,'NPFC_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (866,'SEAFO_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (867,'SPRFMO_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+Insert into ref_cover_page (CD_COVER_PAGE,TITLE_E, IDENTIFIER,SUBJECT,PUBLISHER) 
+values (868,'WECAFC_CEM Conservation and Enforcement measures','IDENTIFIER','SUBJECT','FAO');
+
 
 Insert into fs_collection_cover_page (CD_COLLECTION,CD_COVER_PAGE,ROLE)
 values (7300,861,'factsheet');
