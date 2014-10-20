a63dbd25d2bc39be7eb28caef7bb2dd982ec219c
erikvaningen <erikvaningen@gmail.com>
2014-10-20 14:08:17 +0200

    preparing datacollections for VME

---
+++ vme-sql/rfmo datacollection.sql
@@ -0,0 +1,36 @@
+-- No Wecafc
+-- delete 7300
+
+id: 20010, acronym: "CCAMLR",
+id: 24561, acronym: "GFCM",
+id: 20220, acronym: "NAFO",
+id: 21580, acronym: "NEAFC",
+id: 22140, acronym: "SEAFO",
+id: 24564, acronym: "NPFC",
+id: 22150, acronym: "WECAFC",
+id: 24558, acronym: "SPRFMO",
+
+
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7351,267000,'CCAMLR VME Factsheets','CCAMLR_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7352,267000,'GFCM VME Factsheets','GFCM_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7353,267000,'NAFO VME Factsheets','NAFO_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7354,267000,'NEAFC VME Factsheets','NEAFC_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7355,267000,'SEAFO VME Factsheets','SEAFO_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7356,267000,'NPFC VME Factsheets','NPFC_VME');
+Insert into REF_DATA_COLLECTION (CD_COLLECTION,CD_META,NAME_E,CODE) 
+values (7357,267000,'SPRFMO VME Factsheets','SPRFMO');
+
+Insert into fs_collection_organisation (CD_COLLECTION, CD_INSTITUTE,,ROLE) 
+values (7351, 20010, 3);
+
+
+
+
+
+--7340
