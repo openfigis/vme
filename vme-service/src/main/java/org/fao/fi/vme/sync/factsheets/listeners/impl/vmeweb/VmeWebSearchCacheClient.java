827851ed42e91b68d6bf7a49eb59dd5e362c543f
erikvaningen <erikvaningen@gmail.com>
2014-11-24 16:04:50 +0100

    solving bug where 	asyncHttpClient.close(); was called in the same thread. This was reported by in the logfiles as a mistake.

--- vme-service/src/main/java/org/fao/fi/vme/sync/factsheets/listeners/impl/vmeweb/VmeWebSearchCacheClient.java
+++ vme-service/src/main/java/org/fao/fi/vme/sync/factsheets/listeners/impl/vmeweb/VmeWebSearchCacheClient.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import com.ning.http.client.AsyncCompletionHandler;
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Response;
 
 /**
@@ -28,6 +29,17 @@ public class VmeWebSearchCacheClient {
 
 	public static final String MESSAGE = "VME_CACHE_DELETED_SUCCESS";
 
+	/**
+	 * An instance of this class will cache every HTTP 1.1 connections and close them when the
+	 * AsyncHttpClientConfig.getIdleConnectionTimeoutInMs() expires. This object can hold many persistent connections to
+	 * different host.
+	 * 
+	 * Therefore there is also the @SuppressWarnings("resource"), because the client is closed by this setting after
+	 * REQUEST_TIMEOUT_IN_MS
+	 * 
+	 */
+	public static final int REQUEST_TIMEOUT_IN_MS = 2000;
+
 	protected static final Logger LOG = LoggerFactory.getLogger(VmeWebSearchCacheClient.class);
 	private static final String ERRORMESSAGE = "Did not manage to clear the cach in vme-web search properly";
 
@@ -38,10 +50,14 @@ public class VmeWebSearchCacheClient {
 	 * launches a asynchronous request in order to have the cache cleared.
 	 */
 	public void process(@Observes VmeModelChange vmeModelChange) {
-		final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+		@SuppressWarnings("resource")
+		final AsyncHttpClient asyncHttpClient = new AsyncHttpClient(new AsyncHttpClientConfig.Builder()
+				.setRequestTimeoutInMs(REQUEST_TIMEOUT_IN_MS).build());
+
 		try {
 			String get = vmeWebServer.getServer() + RESOURCE;
 			LOG.info("About to launch async request to clear the cache on " + get);
+
 			asyncHttpClient.prepareGet(get).execute(new AsyncCompletionHandler<Response>() {
 				@Override
 				public Response onCompleted(Response response) throws Exception {
@@ -50,7 +66,6 @@ public class VmeWebSearchCacheClient {
 					}
 					LOG.info("CacheDeleteHandler received this message after a cache delete request was launched to vme-web search : "
 							+ response.getResponseBody());
-					asyncHttpClient.close();
 					return response;
 				}
 
@@ -65,5 +80,4 @@ public class VmeWebSearchCacheClient {
 			LOG.error(ERRORMESSAGE, e);
 		}
 	}
-
 }
