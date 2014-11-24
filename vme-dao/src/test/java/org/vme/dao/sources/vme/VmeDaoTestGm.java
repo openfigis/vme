d11de01e526cff88e2cee475650eefc7a0679a46
erikvaningen <erikvaningen@gmail.com>
2014-11-24 16:09:35 +0100

    adding tests for the relation generalmeasure-informationsource

--- vme-dao/src/test/java/org/vme/dao/sources/vme/VmeDaoTestGm.java
+++ vme-dao/src/test/java/org/vme/dao/sources/vme/VmeDaoTestGm.java
@@ -1,13 +1,18 @@
 package org.vme.dao.sources.vme;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 import org.fao.fi.vme.domain.model.GeneralMeasure;
+import org.fao.fi.vme.domain.model.InformationSource;
+import org.fao.fi.vme.domain.model.Rfmo;
+import org.fao.fi.vme.domain.test.GeneralMeasureMock;
+import org.fao.fi.vme.domain.test.InformationSourceMock;
+import org.fao.fi.vme.domain.test.RfmoMock;
 import org.jglue.cdiunit.ActivatedAlternatives;
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
@@ -25,16 +30,54 @@ public class VmeDaoTestGm {
 	private VmeDao dao;
 
 	@Test
-	public void testDeleteGm() {
-		GeneralMeasure gm = dao.getEntityById(dao.getEm(), GeneralMeasure.class, 24398l);
-		assertNotNull(gm);
-		EntityManager em = dao.getEm();
-		EntityTransaction tx = em.getTransaction();
+	public void testCreateGeneralMeasure() throws Throwable {
 
-		tx.begin();
-		dao.delete(gm);
-		tx.commit();
-		assertNull(dao.getEntityById(dao.getEm(), GeneralMeasure.class, 24398l));
+		Rfmo rfmo = RfmoMock.create();
+
+		GeneralMeasure gm = GeneralMeasureMock.create();
+		gm.setRfmo(rfmo);
+
+		EntityTransaction et = dao.getEm().getTransaction();
+		et.begin();
+		dao.create(gm);
+		et.commit();
+
+		System.out.println(dao.getEm());
+		assertEquals(1, dao.loadObjects(GeneralMeasure.class).size());
+
+	}
+
+	@Test
+	public void testUpdateGmAddInformationSource() throws Throwable {
+
+		GeneralMeasure gm = GeneralMeasureMock.create();
+		InformationSource im1 = InformationSourceMock.create();
+		dao.persist(InformationSourceMock.createInformationSourceType());
+		dao.persist(gm);
+		dao.persist(im1);
+		gm.setInformationSourceList(new ArrayList<InformationSource>());
+		gm.getInformationSourceList().add(im1);
+
+		EntityTransaction et = dao.getEm().getTransaction();
+		et.begin();
+		dao.update(gm);
+		et.commit();
+
+		InformationSource im2 = InformationSourceMock.create();
+		dao.persist(im2);
+		gm.getInformationSourceList().add(im2);
+
+		EntityTransaction et2 = dao.getEm().getTransaction();
+		et2.begin();
+		dao.update(gm);
+
+		dao.getEm().merge(gm);
+		dao.getEm().merge(gm);
+		dao.getEm().merge(gm);
+		et2.commit();
+
+		assertEquals(1, dao.loadObjects(GeneralMeasure.class).size());
+		assertEquals(2, dao.loadObjects(InformationSource.class).size());
 
 	}
 }
