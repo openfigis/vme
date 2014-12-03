a73ae2d47cd25a94016ed199c30c1a5b85c27ea5
erikvaningen <erikvaningen@gmail.com>
2014-12-03 15:32:16 +0100

    failed attemp to reproduce error with multilingual string

--- vme-dao/src/test/java/org/vme/dao/sources/vme/MultiLingualStringPersistenceTest.java
+++ vme-dao/src/test/java/org/vme/dao/sources/vme/MultiLingualStringPersistenceTest.java
@@ -2,10 +2,13 @@ package org.vme.dao.sources.vme;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+
 import javax.inject.Inject;
 
 import org.fao.fi.vme.domain.model.MultiLingualString;
 import org.fao.fi.vme.domain.model.SpecificMeasure;
+import org.fao.fi.vme.domain.util.Lang;
 import org.fao.fi.vme.domain.util.MultiLingualStringUtil;
 import org.jglue.cdiunit.ActivatedAlternatives;
 import org.jglue.cdiunit.AdditionalClasses;
@@ -24,6 +27,35 @@ public class MultiLingualStringPersistenceTest {
 
 	@Inject
 	VmeDao dao;
+
+	/**
+	 * ORA-00001: unique constraintviolated
+	 * 
+	 * Caused by: java.sql.SQLIntegrityConstraintViolationException: ORA-00001: unique constraint (VME.SYS_C006744)
+	 * violated
+	 * 
+	 * Experimented with Insert into multilingualstring_stringmap (MULTILINGUALSTRING_ID,STRINGMAP_KEY) values
+	 * (31463,1); In development db: SQL Error: ORA-00001: unique constraint (VME.SYS_C00135268) violated
+	 * 
+	 * TODO reproduce the above error 
+	 * 
+	 */
+	@Test
+	public void testORA00001uniqueConstraintViolated() {
+
+		MultiLingualString m1 = u.english("piet");
+		dao.persist(m1);
+		System.out.println(m1.getId());
+
+		MultiLingualString m2 = new MultiLingualString();
+
+		m2.setId(m1.getId());
+		m2.setStringMap(new HashMap<Integer, String>());
+		m2.getStringMap().put(Lang.EN, m1.getStringMap().get(Lang.EN));
+		// merge works, persist does however not cause the unique constraint violated error
+		dao.merge(m2);
+
+	}
 
 	/**
 	 * testing the fact that the orphan multi lingual string gets removed when the parent gets removed.
