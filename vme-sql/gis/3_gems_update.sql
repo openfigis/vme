99a40af80760ebfee88501ad25a6140e9ea9bdca
eblondel <emmanuel.blondel1@gmail.com>
2015-10-08 19:04:36 +0200

    modify gis cms sql scripts with proper view naming

--- vme-sql/gis/3_gems_update.sql
+++ vme-sql/gis/3_gems_update.sql
@@ -1,5 +1,7 @@
---Comment Erik run as figis-gis
-CREATE OR REPLACE VIEW FIGIS_GIS.VMEMEASURE_TIME AS
+-- CREATE VIEW for VME GEMS products
+-- ---------------------------------
+CREATE OR REPLACE VIEW FIGIS_GIS.VMEMEASURES_TIME AS
+-- VME_GIS_CLOSURE (from VME-db CMS)
 SELECT
   THE_GEOM,
   VME_ID,
@@ -13,20 +15,47 @@ SELECT
   REG_NAME,
   SURFACE
 FROM
-  FIGIS_GIS.VMEMEASURE;
--- Report Erik: done in figis-gis-dev
--- Report Erik: done in figis-gis-qa
+  FIGIS_GIS.VME_GIS_CLOSURE
+UNION
+-- VME_GIS_BFA (still manually produced)
+SELECT
+  THE_GEOM,
+  VME_ID,
+  OWNER,
+  TO_DATE((YEAR || '01-01'),'YYYY-MM-DD') AS START_YEAR,
+  TO_DATE((END_YEAR || '12-31'), 'YYYY-MM-DD') AS END_YEAR,
+  LOCAL_NAME,
+  GLOB_TYPE,
+  GLOB_NAME,
+  REG_TYPE,
+  REG_NAME,
+  SURFACE
+FROM
+  FIGIS_GIS.VME_GIS_BFA
+-- VME_GIS_OARA (expected to be produced through an automated geoprocess)
+SELECT
+  THE_GEOM,
+  VME_ID,
+  OWNER,
+  TO_DATE((YEAR || '01-01'),'YYYY-MM-DD') AS START_YEAR,
+  TO_DATE((END_YEAR || '12-31'), 'YYYY-MM-DD') AS END_YEAR,
+  LOCAL_NAME,
+  GLOB_TYPE,
+  GLOB_NAME,
+  REG_TYPE,
+  REG_NAME,
+  SURFACE
+FROM
+  FIGIS_GIS.VME_GIS_OARA;
+
   
--- Need to insert GIS metadata (first try delete in case existing record)
-DELETE FROM "MDSYS"."USER_SDO_GEOM_METADATA" WHERE TABLE_NAME = 'VMEMEASURE_TIME';
+-- Need to insert GIS metadata
+DELETE FROM MDSYS.USER_SDO_GEOM_METADATA WHERE TABLE_NAME = 'VMEMEASURES_TIME';
 INSERT INTO
   MDSYS.USER_SDO_GEOM_METADATA
 VALUES (
-  'VMEMEASURE_TIME',
+  'VMEMEASURES_TIME',
   'THE_GEOM',
   MDSYS.SDO_DIM_ARRAY( MDSYS.SDO_DIM_ELEMENT('X',-180,180,0.005), MDSYS.SDO_DIM_ELEMENT('Y',-90,90,0.005) ),
   4326
 );
-commit;
--- Report Erik: done in figis-gis-dev
--- Report Erik: done in figis-gis-qa
